5d97d9d7ff4898b3ae04db248fc9db072830bf5a
WuHaobo <wuhaobo1994@gmail.com>
2020-04-10 04:02:25 +0800

    make readthedocs available

--- docs/conf.py
+++ docs/conf.py
@@ -63,11 +63,15 @@ language = 'zh'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 
+# on_rtd is whether we are on readthedocs.org, this line of code grabbed from docs.readthedocs.org
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-if not on_rtd:
+
+if not on_rtd:  # only import and set the theme if we're building docs locally
     import sphinx_rtd_theme
-    html_theme = "sphinx_rtd_theme"
+    html_theme = 'sphinx_rtd_theme'
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+# otherwise, readthedocs.org uses their theme by default, so no need to specify it
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
