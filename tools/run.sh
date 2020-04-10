6e23365d8700cdb40190cebeeb261b58263333c4
WuHaobo <wuhaobo1994@gmail.com>
2020-04-11 02:53:55 +0800

    polish run.sh

---
+++ tools/run.sh
@@ -0,0 +1,7 @@
+export PYTHONPATH=$(dirname "$PWD"):$PWD:$PYTHONPATH
+
+python -m paddle.distributed.launch \
+    --selected_gpus="0,1,2,3" \
+    --log_dir=log_ResNet50 \
+    tools/train.py \
+        -c ./configs/ResNet/ResNet50.yaml
