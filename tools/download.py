a6b0f49cacde850294c5695ee9b17bfde1e8b692
littletomatodonkey <dazhiningsibuqu@163.com>
2020-05-04 02:00:48 +0000

    add pretrain list show

--- tools/download.py
+++ tools/download.py
@@ -1,18 +1,17 @@
 # Copyright (c) 2020 PaddlePaddle Authors. All Rights Reserve.
 #
-#Licensed under the Apache License, Version 2.0 (the "License");
-#you may not use this file except in compliance with the License.
-#You may obtain a copy of the License at
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
 #    http://www.apache.org/licenses/LICENSE-2.0
 #
-#Unless required by applicable law or agreed to in writing, software
-#distributed under the License is distributed on an "AS IS" BASIS,
-#WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#See the License for the specific language governing permissions and
-#limitations under the License.
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-import sys
 import argparse
 
 from ppcls import model_zoo
@@ -26,6 +25,7 @@ def parse_args():
     parser.add_argument('-a', '--architecture', type=str, default='ResNet50')
     parser.add_argument('-p', '--path', type=str, default='./pretrained/')
     parser.add_argument('-d', '--decompress', type=str2bool, default=True)
+    parser.add_argument('-l', '--list', type=str2bool, default=False)
 
     args = parser.parse_args()
     return args
@@ -33,7 +33,10 @@ def parse_args():
 
 def main():
     args = parse_args()
-    model_zoo.get(args.architecture, args.path, args.decompress)
+    if args.list:
+        model_zoo.list_models()
+    else:
+        model_zoo.get(args.architecture, args.path, args.decompress)
 
 
 if __name__ == '__main__':
