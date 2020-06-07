ed2f71ca68267b8f90fea952098a68fbfb7bee77
WuHaobo <wuhaobo1994@gmail.com>
2020-06-07 15:39:26 +0800

    dygraph optimizer

--- ppcls/optimizer/optimizer.py
+++ ppcls/optimizer/optimizer.py
@@ -1,16 +1,16 @@
-#copyright (c) 2020 PaddlePaddle Authors. All Rights Reserve.
+# copyright (c) 2020 PaddlePaddle Authors. All Rights Reserve.
 #
-#Licensed under the Apache License, Version 2.0 (the "License");
-#you may not use this file except in compliance with the License.
-#You may obtain a copy of the License at
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
 #    http://www.apache.org/licenses/LICENSE-2.0
 #
-#Unless required by applicable law or agreed to in writing, software
-#distributed under the License is distributed on an "AS IS" BASIS,
-#WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#See the License for the specific language governing permissions and
-#limitations under the License.
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 from __future__ import absolute_import
 from __future__ import division
@@ -48,6 +48,8 @@ class OptimizerBuilder(object):
             reg = getattr(pfreg, reg_func)(reg_factor)
             self.params['regularization'] = reg
 
-    def __call__(self, learning_rate):
+    def __call__(self, learning_rate, parameter_list):
         opt = getattr(pfopt, self.function)
-        return opt(learning_rate=learning_rate, **self.params)
+        return opt(learning_rate=learning_rate,
+                   parameter_list=parameter_list,
+                   **self.params)
