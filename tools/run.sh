32dc1c1c0d37bba769d4d706215a993204a445bc
littletomatodonkey <dazhiningsibuqu@163.com>
2020-09-02 15:15:49 +0000

    improve dygraph model

--- tools/run.sh
+++ tools/run.sh
@@ -5,4 +5,5 @@ export PYTHONPATH=$PWD:$PYTHONPATH
 python -m paddle.distributed.launch \
     --selected_gpus="0,1,2,3" \
     tools/train.py \
-        -c ./configs/ResNet/ResNet50.yaml
+        -c ./configs/ResNet/ResNet50_vd.yaml \
+        -o print_interval=10
