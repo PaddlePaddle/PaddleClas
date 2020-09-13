d29700944bfd1dc43162ac8b954905a42e75a6c8
littletomatodonkey <dazhiningsibuqu@163.com>
2020-09-13 09:57:23 +0000

    fix eval

--- tools/eval.py
+++ tools/eval.py
@@ -12,24 +12,21 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from __future__ import absolute_import
-import program
-from ppcls.utils import logger
-from ppcls.utils.save_load import init_model
-from ppcls.utils.config import get_config
-from ppcls.data import Reader
-import paddle.fluid as fluid
-import paddle
 import argparse
-from __future__ import division
-from __future__ import print_function
-
 import os
 import sys
 __dir__ = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(__dir__)
 sys.path.append(os.path.abspath(os.path.join(__dir__, '..')))
 
+import program
+from ppcls.data import Reader
+from ppcls.utils.config import get_config
+from ppcls.utils.save_load import init_model
+from ppcls.utils import logger
+
+import paddle
+from paddle.distributed import ParallelEnv
 
 def parse_args():
     parser = argparse.ArgumentParser("PaddleClas eval script")
@@ -54,20 +51,22 @@ def main(args):
     # assign place
     use_gpu = config.get("use_gpu", True)
     if use_gpu:
-        gpu_id = fluid.dygraph.ParallelEnv().dev_id
-        place = fluid.CUDAPlace(gpu_id)
+        gpu_id = ParallelEnv().dev_id
+        place = paddle.CUDAPlace(gpu_id)
     else:
-        place = fluid.CPUPlace()
-    with fluid.dygraph.guard(place):
-        strategy = fluid.dygraph.parallel.prepare_context()
-        net = program.create_model(config.ARCHITECTURE, config.classes_num)
-        net = fluid.dygraph.parallel.DataParallel(net, strategy)
-        init_model(config, net, optimizer=None)
-        valid_dataloader = program.create_dataloader()
-        valid_reader = Reader(config, 'valid')()
-        valid_dataloader.set_sample_list_generator(valid_reader, place)
-        net.eval()
-        top1_acc = program.run(valid_dataloader, config, net, None, 0, 'valid')
+        place = paddle.CPUPlace()
+
+    paddle.disable_static(place)
+
+    strategy = paddle.distributed.init_parallel_env()
+    net = program.create_model(config.ARCHITECTURE, config.classes_num)
+    net = paddle.DataParallel(net, strategy)
+    init_model(config, net, optimizer=None)
+    valid_dataloader = program.create_dataloader()
+    valid_reader = Reader(config, 'valid')()
+    valid_dataloader.set_sample_list_generator(valid_reader, place)
+    net.eval()
+    top1_acc = program.run(valid_dataloader, config, net, None, 0, 'valid')
 
 
 if __name__ == '__main__':
