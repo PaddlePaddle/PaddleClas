922f33fef10d89e4c8b28c38bd248aa0c5613a43
littletomatodonkey <dazhiningsibuqu@163.com>
2020-09-16 08:35:37 +0000

    fix train

--- tools/train.py
+++ tools/train.py
@@ -98,7 +98,7 @@ def main(args):
             # 2. validate with validate dataset
             if config.validate and epoch_id % config.valid_interval == 0:
                 net.eval()
-                top1_acc = program.run(valid_dataloader, config, net, None,
+                top1_acc = program.run(valid_dataloader, config, net, None, None,
                                        epoch_id, 'valid')
                 if top1_acc > best_top1_acc:
                     best_top1_acc = top1_acc
