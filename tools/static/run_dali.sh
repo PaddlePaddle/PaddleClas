1e4925704d7f41ac233dd0e8e87f1ebe9365c954
littletomatodonkey <2120160898@bit.edu.cn>
2020-12-03 15:48:35 +0800

    fix dali train (#446)

--- tools/static/run_dali.sh
+++ tools/static/run_dali.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-export CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7"
+export CUDA_VISIBLE_DEVICES="0,1,2,3"
 export FLAGS_fraction_of_gpu_memory_to_use=0.80
 
-python -m paddle.distributed.launch \
-    --selected_gpus="0,1,2,3,4,5,6,7" \
-    tools/train.py \
+python3.7 -m paddle.distributed.launch \
+    --selected_gpus="0,1,2,3" \
+    tools/static/train.py \
         -c ./configs/ResNet/ResNet50.yaml \
         -o print_interval=10 \
         -o use_dali=true
