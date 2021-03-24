f3c9eec43bcce130cbd062a7ef5074d73587f77d
lyuwenyu <wenyu.lyu@gmail.com>
2021-03-24 16:37:50 +0800

    add more example

--- hubconf.py
+++ hubconf.py
@@ -1,13 +1,30 @@
 
 
 
-from ppcls.modeling.architectures.resnet import ResNet18
+from ppcls.modeling.architectures.resnet import ResNet18 as _ResNet18
+from ppcls.modeling.architectures.resnet import ResNet34 as _ResNet34
+from ppcls.modeling.architectures.resnet import ResNet50 as _ResNet50
 
 
-def ResNet18Test(**args):
-    '''lyuwenyu/PaddleClas/hub_L
-    ResNet18Entry test
+
+def ResNet18(**kwargs):
+    '''ResNet18
     '''
-    model = ResNet18(**args)
+    model = _ResNet18(**kwargs)
     return model
 
+
+
+def ResNet34(**kwargs):
+    '''ResNet34
+    '''
+    model = _ResNet18(**kwargs)
+    return model
+
+
+
+def ResNet50(**kwargs):
+    '''ResNet50
+    '''
+    model = _ResNet18(**kwargs)
+    return model
