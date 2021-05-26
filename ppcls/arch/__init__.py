85219aa43794d3cb93aed05433e4bee4b459a1b4
littletomatodonkey <dazhiningsibuqu@163.com>
2021-05-26 02:22:44 +0000

    add arch init

--- ppcls/arch/__init__.py
+++ ppcls/arch/__init__.py
@@ -12,8 +12,48 @@
 #See the License for the specific language governing permissions and
 #limitations under the License.
 
+import sys
+import copy
 from . import backbone
 
 from .backbone import *
 from ppcls.arch.loss_metrics.loss import *
 from .utils import *
+
+
+def build_arch(config):
+    config = copy.deepcopy(config)
+    arch_type = config.pop("type")
+    module = sys.modules[__name__]
+    arch = getattr(module)(config)
+    return arch
+
+
+class BaseModel(nn.Layer):
+    def __init__(self, config):
+        super().__init__()
+        backbone_config = config["Backbone"]
+        backbone_type = backbone_config.pop("type")
+        self.backbone = getattr(backbone)(**backbone_config)
+
+        if "Neck" in config:
+            neck_config = config["Neck"]
+            neck_type = neck_config.pop("type")
+            self.neck = getattr(backbone)(**neck_config)
+        else:
+            self.neck = None
+
+        if "Head" in config:
+            head_config = config["Head"]
+            head_type = head_config.pop("type")
+            self.head = getattr(head)(**head_config)
+        else:
+            self.head = None
+
+    def forward(self, x):
+        y = self.backbone(x)
+        if self.neck is not None:
+            y = self.neck(y)
+        if self.head is not None:
+            y = self.head(y)
+        return y
