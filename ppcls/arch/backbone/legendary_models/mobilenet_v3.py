78736e6ee8e1d0a56f82acbe34f8fd081a939bcb
dongshuilong <dongshuilong@baidu.com>
2021-05-26 20:09:13 +0800

    update mobienet_v3

--- ppcls/arch/backbone/legendary_models/mobilenet_v3.py
+++ ppcls/arch/backbone/legendary_models/mobilenet_v3.py
@@ -1,4 +1,4 @@
-# copyright (c) 2020 PaddlePaddle Authors. All Rights Reserve.
+# copyright (c) 2021 PaddlePaddle Authors. All Rights Reserve.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -79,6 +79,254 @@ LAST_SECOND_CONV_SMALL = 576
 LAST_SECOND_CONV_LARGE = 960
 # last conv output channel number for 'large' and 'small'
 LAST_CONV = 1280
+
+
+def make_divisible(v, divisor=8, min_value=None):
+    if min_value is None:
+        min_value = divisor
+    new_v = max(min_value, int(v + divisor / 2) // divisor * divisor)
+    if new_v < 0.9 * v:
+        new_v += divisor
+    return new_v
+
+
+def create_act(act):
+    if act == 'hardswish':
+        return nn.Hardswish()
+    elif act == 'relu':
+        return nn.ReLU()
+    elif act is None:
+        return None
+    else:
+        raise RuntimeError(
+            'The activation function is selected incorrectly: {}'.format(act))
+
+
+class MobileNetV3(TheseusLayer):
+    """
+    MobileNetV3
+    Args:
+        config: list. MobileNetV3 depthwise blocks config.
+        scale: float=1.0. The coefficient that controls the size of network parameters. 
+        class_num: int=1000. The number of classes.
+        inplanes: int=16. The output channel number of first convolution layer.
+        class_squeeze: int=960. The output channel number of second to last convolution layer. 
+        class_expand: int=1280. The output channel number of last convolution layer. 
+        dropout_prob: float=0.2.  Probability of setting units to zero.
+    Returns:
+        model: nn.Layer. Specific MobileNetV3 model depends on args.
+    """
+
+    def __init__(self,
+                 config,
+                 scale=1.0,
+                 class_num=1000,
+                 inplanes=STEM_CONV_NUMBER,
+                 class_squeeze=LAST_SECOND_CONV_LARGE,
+                 class_expand=LAST_CONV,
+                 dropout_prob=0.2):
+        super(MobileNetV3, self).__init__()
+
+        self.cfg = config
+        self.scale = scale
+        self.inplanes = inplanes
+        self.class_squeeze = class_squeeze
+        self.class_expand = class_expand
+        self.class_num = class_num
+
+        self.conv1 = ConvBNLayer(
+            in_c=3,
+            out_c=make_divisible(self.inplanes * self.scale),
+            filter_size=3,
+            stride=2,
+            padding=1,
+            num_groups=1,
+            if_act=True,
+            act="hardswish")
+
+        self.blocks = nn.Sequential(*[
+            ResidualUnit(
+                in_c=make_divisible(self.inplanes * self.scale if i == 0 else
+                                    self.cfg[i - 1][2] * self.scale),
+                mid_c=make_divisible(self.scale * exp),
+                out_c=make_divisible(self.scale * c),
+                filter_size=k,
+                stride=s,
+                use_se=se,
+                act=act) for i, (k, exp, c, se, act, s) in enumerate(self.cfg)
+        ])
+
+        self.last_second_conv = ConvBNLayer(
+            in_c=make_divisible(self.cfg[-1][2] * self.scale),
+            out_c=make_divisible(self.scale * self.class_squeeze),
+            filter_size=1,
+            stride=1,
+            padding=0,
+            num_groups=1,
+            if_act=True,
+            act="hardswish")
+
+        self.pool = AdaptiveAvgPool2D(1)
+
+        self.last_conv = Conv2D(
+            in_channels=make_divisible(self.scale * self.class_squeeze),
+            out_channels=self.class_expand,
+            kernel_size=1,
+            stride=1,
+            padding=0,
+            bias_attr=False)
+
+        self.hardswish = nn.Hardswish()
+        self.dropout = Dropout(p=dropout_prob, mode="downscale_in_infer")
+        self.flatten = nn.Flatten(start_axis=1, stop_axis=-1)
+
+        self.out = Linear(self.class_expand, class_num)
+
+    def forward(self, x):
+        x = self.conv1(x)
+
+        x = self.blocks(x)
+
+        x = self.last_second_conv(x)
+        x = self.pool(x)
+
+        x = self.last_conv(x)
+        x = self.hardswish(x)
+        x = self.dropout(x)
+        x = self.flatten(x)
+        x = self.out(x)
+
+        return x
+
+
+class ConvBNLayer(TheseusLayer):
+    def __init__(self,
+                 in_c,
+                 out_c,
+                 filter_size,
+                 stride,
+                 padding,
+                 num_groups=1,
+                 if_act=True,
+                 act=None):
+        super(ConvBNLayer, self).__init__()
+
+        self.conv = Conv2D(
+            in_channels=in_c,
+            out_channels=out_c,
+            kernel_size=filter_size,
+            stride=stride,
+            padding=padding,
+            groups=num_groups,
+            bias_attr=False)
+        self.bn = BatchNorm(
+            num_channels=out_c,
+            act=None,
+            param_attr=ParamAttr(regularizer=L2Decay(0.0)),
+            bias_attr=ParamAttr(regularizer=L2Decay(0.0)))
+        self.if_act = if_act
+        self.act = create_act(act)
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.bn(x)
+        if self.if_act:
+            x = self.act(x)
+        return x
+
+
+class ResidualUnit(TheseusLayer):
+    def __init__(self,
+                 in_c,
+                 mid_c,
+                 out_c,
+                 filter_size,
+                 stride,
+                 use_se,
+                 act=None):
+        super(ResidualUnit, self).__init__()
+        self.if_shortcut = stride == 1 and in_c == out_c
+        self.if_se = use_se
+
+        self.expand_conv = ConvBNLayer(
+            in_c=in_c,
+            out_c=mid_c,
+            filter_size=1,
+            stride=1,
+            padding=0,
+            if_act=True,
+            act=act)
+        self.bottleneck_conv = ConvBNLayer(
+            in_c=mid_c,
+            out_c=mid_c,
+            filter_size=filter_size,
+            stride=stride,
+            padding=int((filter_size - 1) // 2),
+            num_groups=mid_c,
+            if_act=True,
+            act=act)
+        if self.if_se:
+            self.mid_se = SEModule(mid_c)
+        self.linear_conv = ConvBNLayer(
+            in_c=mid_c,
+            out_c=out_c,
+            filter_size=1,
+            stride=1,
+            padding=0,
+            if_act=False,
+            act=None)
+
+    def forward(self, x):
+        identity = x
+        x = self.expand_conv(x)
+        x = self.bottleneck_conv(x)
+        if self.if_se:
+            x = self.mid_se(x)
+        x = self.linear_conv(x)
+        if self.if_shortcut:
+            x = paddle.add(identity, x)
+        return x
+
+
+# nn.Hardsigmoid can't transfer 'slope' and 'offset' in nn.functional.hardsigmoid
+class Hardsigmoid(TheseusLayer):
+    def __init__(self, slope=0.2, offset=0.5):
+        super(Hardsigmoid, self).__init__()
+        self.slope = slope
+        self.offset = offset
+
+    def forward(self, x):
+        return nn.functional.hardsigmoid(
+            x, slope=self.slope, offset=self.offset)
+
+
+class SEModule(TheseusLayer):
+    def __init__(self, channel, reduction=4):
+        super(SEModule, self).__init__()
+        self.avg_pool = AdaptiveAvgPool2D(1)
+        self.conv1 = Conv2D(
+            in_channels=channel,
+            out_channels=channel // reduction,
+            kernel_size=1,
+            stride=1,
+            padding=0)
+        self.relu = nn.ReLU()
+        self.conv2 = Conv2D(
+            in_channels=channel // reduction,
+            out_channels=channel,
+            kernel_size=1,
+            stride=1,
+            padding=0)
+        self.hardsigmoid = Hardsigmoid(slope=0.2, offset=0.5)
+
+    def forward(self, x):
+        identity = x
+        x = self.avg_pool(x)
+        x = self.conv1(x)
+        x = self.relu(x)
+        x = self.conv2(x)
+        x = self.hardsigmoid(x)
+        return paddle.multiply(x=identity, y=x)
 
 
 def MobileNetV3_small_x0_35(**args):
@@ -169,262 +417,3 @@ def MobileNetV3_large_x1_25(**args):
         class_squeeze=LAST_SECOND_CONV_LARGE,
         **args)
     return model
-
-
-def make_divisible(v, divisor=8, min_value=None):
-    if min_value is None:
-        min_value = divisor
-    new_v = max(min_value, int(v + divisor / 2) // divisor * divisor)
-    if new_v < 0.9 * v:
-        new_v += divisor
-    return new_v
-
-
-def create_act(act):
-    if act == 'hardswish':
-        return nn.Hardswish()
-    elif act == 'relu':
-        return nn.ReLU()
-    elif act is None:
-        return None
-    else:
-        raise RuntimeError(
-            'The activation function is selected incorrectly: {}'.format(act))
-
-
-class MobileNetV3(TheseusLayer):
-    """
-    MobileNetV3
-    Args:
-        config: list. MobileNetV3 depthwise blocks config.
-        scale: float=1.0. The coefficient that controls the size of network parameters. 
-        class_num: int=1000. The number of classes.
-        inplanes: int=16. The output channel number of first convolution layer.
-        class_squeeze: int=960. The output channel number of second to last convolution layer. 
-        class_expand: int=1280. The output channel number of last convolution layer. 
-        dropout_prob: float=0.2.  Probability of setting units to zero.
-    Returns:
-        model: nn.Layer. Specific MobileNetV3 model depends on args.
-    """
-
-    def __init__(self,
-                 config,
-                 scale=1.0,
-                 class_num=1000,
-                 inplanes=STEM_CONV_NUMBER,
-                 class_squeeze=LAST_SECOND_CONV_LARGE,
-                 class_expand=LAST_CONV,
-                 dropout_prob=0.2):
-        super(MobileNetV3, self).__init__()
-
-        self.cfg = config
-        self.scale = scale
-        self.inplanes = inplanes
-        self.class_squeeze = class_squeeze
-        self.class_expand = class_expand
-        self.class_num = class_num
-
-        self.conv1 = ConvBNLayer(
-            in_c=3,
-            out_c=make_divisible(self.inplanes * self.scale),
-            filter_size=3,
-            stride=2,
-            padding=1,
-            num_groups=1,
-            if_act=True,
-            act="hardswish")
-        #  name="conv1")
-
-        self.blocks = nn.Sequential(*[
-            ResidualUnit(
-                in_c=make_divisible(self.inplanes * self.scale if i == 0 else
-                                    self.cfg[i - 1][2] * self.scale),
-                mid_c=make_divisible(self.scale * exp),
-                out_c=make_divisible(self.scale * c),
-                filter_size=k,
-                stride=s,
-                use_se=se,
-                act=act) for i, (k, exp, c, se, act, s) in enumerate(self.cfg)
-        ])
-
-        self.last_second_conv = ConvBNLayer(
-            in_c=make_divisible(self.cfg[-1][2] * self.scale),
-            out_c=make_divisible(self.scale * self.class_squeeze),
-            filter_size=1,
-            stride=1,
-            padding=0,
-            num_groups=1,
-            if_act=True,
-            act="hardswish")
-
-        self.pool = AdaptiveAvgPool2D(1)
-
-        self.last_conv = Conv2D(
-            in_channels=make_divisible(self.scale * self.class_squeeze),
-            out_channels=self.class_expand,
-            kernel_size=1,
-            stride=1,
-            padding=0,
-            #  weight_attr=ParamAttr(name="last_1x1_conv_weights"),
-            bias_attr=False)
-
-        self.hardswish = nn.Hardswish()
-        self.dropout = Dropout(p=dropout_prob, mode="downscale_in_infer")
-        self.flatten = nn.Flatten(start_axis=1, stop_axis=-1)
-
-        self.out = Linear(self.class_expand, class_num)
-        #  weight_attr=ParamAttr("fc_weights"),
-        #  bias_attr=ParamAttr(name="fc_offset"))
-
-    def forward(self, x):
-        x = self.conv1(x)
-
-        x = self.blocks(x)
-
-        x = self.last_second_conv(x)
-        x = self.pool(x)
-
-        x = self.last_conv(x)
-        x = self.hardswish(x)
-        x = self.dropout(x)
-        x = self.flatten(x)
-        x = self.out(x)
-
-        return x
-
-
-class ConvBNLayer(TheseusLayer):
-    def __init__(self,
-                 in_c,
-                 out_c,
-                 filter_size,
-                 stride,
-                 padding,
-                 num_groups=1,
-                 if_act=True,
-                 act=None,
-                 use_cudnn=True):
-        super(ConvBNLayer, self).__init__()
-
-        self.conv = Conv2D(
-            in_channels=in_c,
-            out_channels=out_c,
-            kernel_size=filter_size,
-            stride=stride,
-            padding=padding,
-            groups=num_groups,
-            #  weight_attr=ParamAttr(name=name + "_weights"),
-            bias_attr=False)
-        self.bn = BatchNorm(
-            num_channels=out_c,
-            act=None,
-            param_attr=ParamAttr(regularizer=L2Decay(0.0)),
-            bias_attr=ParamAttr(regularizer=L2Decay(0.0)))
-        #  moving_mean_name=name + "_bn_mean",
-        #  moving_variance_name=name + "_bn_variance")
-        self.if_act = if_act
-        self.act = create_act(act)
-
-    def forward(self, x):
-        x = self.conv(x)
-        x = self.bn(x)
-        if self.if_act:
-            x = self.act(x)
-        return x
-
-
-class ResidualUnit(TheseusLayer):
-    def __init__(self,
-                 in_c,
-                 mid_c,
-                 out_c,
-                 filter_size,
-                 stride,
-                 use_se,
-                 act=None):
-        super(ResidualUnit, self).__init__()
-        self.if_shortcut = stride == 1 and in_c == out_c
-        self.if_se = use_se
-
-        self.expand_conv = ConvBNLayer(
-            in_c=in_c,
-            out_c=mid_c,
-            filter_size=1,
-            stride=1,
-            padding=0,
-            if_act=True,
-            act=act)
-        self.bottleneck_conv = ConvBNLayer(
-            in_c=mid_c,
-            out_c=mid_c,
-            filter_size=filter_size,
-            stride=stride,
-            padding=int((filter_size - 1) // 2),
-            num_groups=mid_c,
-            if_act=True,
-            act=act)
-        if self.if_se:
-            self.mid_se = SEModule(mid_c)
-        self.linear_conv = ConvBNLayer(
-            in_c=mid_c,
-            out_c=out_c,
-            filter_size=1,
-            stride=1,
-            padding=0,
-            if_act=False,
-            act=None)
-        #  name=name + "_linear")
-
-    def forward(self, inputs):
-        x = self.expand_conv(inputs)
-        x = self.bottleneck_conv(x)
-        if self.if_se:
-            x = self.mid_se(x)
-        x = self.linear_conv(x)
-        if self.if_shortcut:
-            x = paddle.add(inputs, x)
-        return x
-
-
-# nn.Hardsigmoid can't transfer 'slope' and 'offset' in nn.functional.hardsigmoid
-class Hardsigmoid(TheseusLayer):
-    def __init__(self, slope=0.2, offset=0.5):
-        super(Hardsigmoid, self).__init__()
-        self.slope = slope
-        self.offset = offset
-
-    def forward(self, x):
-        return nn.functional.hardsigmoid(
-            x, slope=self.slope, offset=self.offset)
-
-
-class SEModule(TheseusLayer):
-    def __init__(self, channel, reduction=4):
-        super(SEModule, self).__init__()
-        self.avg_pool = AdaptiveAvgPool2D(1)
-        self.conv1 = Conv2D(
-            in_channels=channel,
-            out_channels=channel // reduction,
-            kernel_size=1,
-            stride=1,
-            padding=0)
-        #  weight_attr=ParamAttr(name=name + "_1_weights"),
-        #  bias_attr=ParamAttr(name=name + "_1_offset"))
-        self.relu = nn.ReLU()
-        self.conv2 = Conv2D(
-            in_channels=channel // reduction,
-            out_channels=channel,
-            kernel_size=1,
-            stride=1,
-            padding=0)
-        #  weight_attr=ParamAttr(name + "_2_weights"),
-        #  bias_attr=ParamAttr(name=name + "_2_offset"))
-        self.hardsigmoid = Hardsigmoid(slope=0.2, offset=0.5)
-
-    def forward(self, inputs):
-        outputs = self.avg_pool(inputs)
-        outputs = self.conv1(outputs)
-        outputs = self.relu(outputs)
-        outputs = self.conv2(outputs)
-        outputs = self.hardsigmoid(outputs)
-        return paddle.multiply(x=inputs, y=outputs)
