f2887bdb690d7ba514c748e03ce16336262f71dc
Felix <freehuang100@163.com>
2021-06-01 11:20:25 +0800

    Create config.h

---
+++ deploy/vector_search/src/config.h
@@ -0,0 +1,18 @@
+#pragma once
+
+
+typedef float value_t; 
+//typedef double dist_t; 
+typedef float dist_t;
+typedef size_t idx_t;
+typedef int UINT;
+
+
+#define ACC_BATCH_SIZE 4096
+#define FIXED_DEGREE 31
+#define FIXED_DEGREE_SHIFT 5
+
+
+//for construction
+#define SEARCH_DEGREE 15
+#define CONSTRUCT_SEARCH_BUDGET 150
