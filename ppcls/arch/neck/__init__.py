9c395ac802d0bbca9e24bbe9db780e48029514dc
Bin Lu <lubing02@gmail.com>
2021-06-01 09:52:56 +0800

    Update __init__.py

--- ppcls/arch/neck/__init__.py
+++ ppcls/arch/neck/__init__.py
@@ -17,7 +17,7 @@ __all__ = ['build_neck"]
 def build_neck(config):
     support_dict = ['FPN', 'FC']
     module_name = config.pop('name')
-    assert module_name in support_dict, Exception('head only support {}'.format(
+    assert module_name in support_dict, Exception('neck only support {}'.format(
         support_dict))
     module_class = eval(module_name)(**config)
     return module_class
