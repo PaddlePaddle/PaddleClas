505c9309a0183d10cd3df82c5baea1fd6d07fa6c
dongshuilong <dongshuilong@baidu.com>
2021-06-04 14:54:34 +0800

    add vehicle neck

--- ppcls/arch/__init__.py
+++ ppcls/arch/__init__.py
@@ -21,7 +21,7 @@ from . import backbone
 from . import gears
 
 from .backbone import *
-from .gears import *
+from .gears import build_gear
 from .utils import *
 
 __all__ = ["build_model", "RecModel"]
@@ -38,34 +38,27 @@ def build_model(config):
 class RecModel(nn.Layer):
     def __init__(self, **config):
         super().__init__()
-
         backbone_config = config["Backbone"]
         backbone_name = backbone_config.pop("name")
-        self.backbone = eval(backbone_name)(**backbone_config)
+        self.backbone = getattr(backbone_name)(**backbone_config)
+        if "BackboneStopLayer" in config:
+            backbone_stop_layer = config["BackboneStopLayer"]
+            self.backbone.stop_layer(backbone_stop_layer)
 
-        assert "Stoplayer" in config, "Stoplayer should be specified in retrieval task \
-                please specified a Stoplayer config"
-
-        stop_layer_config = config["Stoplayer"]
-        self.backbone.stop_after(stop_layer_config["name"])
-
-        if stop_layer_config.get("embedding_size", 0) > 0:
-            self.neck = nn.Linear(stop_layer_config["output_dim"],
-                                  stop_layer_config["embedding_size"])
-            embedding_size = stop_layer_config["embedding_size"]
+        if "Neck" in config:
+            self.neck = build_gear(config["Neck"])
         else:
             self.neck = None
-            embedding_size = stop_layer_config["output_dim"]
 
-        assert "Head" in config, "Head should be specified in retrieval task \
-                please specify a Head config"
+        if "Head" in config:
+            self.head = build_gear(config["Head"])
+        else:
+            self.head = None
 
-        config["Head"]["embedding_size"] = embedding_size
-        self.head = build_head(config["Head"])
-
-    def forward(self, x, label):
-        x = self.backbone(x)
+    def forward(self, x):
+        y = self.backbone(x)
         if self.neck is not None:
-            x = self.neck(x)
-        y = self.head(x, label)
-        return {"features": x, "logits": y}
+            y = self.neck(y)
+        if self.head is not None:
+            y = self.head(y)
+        return y
