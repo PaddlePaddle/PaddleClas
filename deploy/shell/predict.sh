9a845e085eef84bcae2c150cc0947468a2307cea
Felix <freehuang100@163.com>
2021-06-05 16:46:14 +0800

    Update predict.sh

--- deploy/shell/predict.sh
+++ deploy/shell/predict.sh
@@ -7,5 +7,5 @@ python3.7 python/predict_cls.py -c configs/inference_cls.yaml
 # detection
 # python3.7 python/predict_det.py -c configs/inference_rec.yaml
 
-# mainbody detection + feature extractor
+# mainbody detection + feature extractor + 
 # python3.7 python/predict_system.py -c configs/inference_rec.yaml 
