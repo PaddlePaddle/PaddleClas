819f44db919c8f62dcd16d89c2d4c27b449f543c
Felix <freehuang100@163.com>
2021-06-07 15:45:02 +0800

    Update __init__.py

--- ppcls/data/__init__.py
+++ ppcls/data/__init__.py
@@ -25,6 +25,7 @@ from ppcls.data.dataloader.imagenet_dataset import ImageNetDataset
 from ppcls.data.dataloader.multilabel_dataset import MultiLabelDataset
 from ppcls.data.dataloader.common_dataset import create_operators
 from ppcls.data.dataloader.vehicle_dataset import CompCars, VeriWild
+from ppcls.data.dataloader.logo_dataset import LogoDataset
 
 # sampler
 from ppcls.data.dataloader.DistributedRandomIdentitySampler import DistributedRandomIdentitySampler
