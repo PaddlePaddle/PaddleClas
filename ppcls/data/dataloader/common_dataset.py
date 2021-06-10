ff6ed305f143e71da1dd24e9d8121c9fd34c9c22
littletomatodonkey <dazhiningsibuqu@163.com>
2021-06-10 12:52:06 +0000

    fix imread

--- ppcls/data/dataloader/common_dataset.py
+++ ppcls/data/dataloader/common_dataset.py
@@ -63,8 +63,8 @@ class CommonDataset(Dataset):
 
     def __getitem__(self, idx):
         try:
-            img = cv2.imread(self.images[idx])
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            with open(self.images[idx], 'rb') as f:
+                img = f.read()
             if self._transform_ops:
                 img = transform(img, self._transform_ops)
             img = img.transpose((2, 0, 1))
