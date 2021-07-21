ae768d6aeea9c6de41c10b3b2c1febe3db0922df
gaotingquan <gaotingquan@baidu.com>
2021-07-21 04:51:45 +0000

    feat: support to enable mixup and cutmix at same time

--- ppcls/data/preprocess/batch_ops/batch_operators.py
+++ ppcls/data/preprocess/batch_ops/batch_operators.py
@@ -23,6 +23,7 @@ from ppcls.data.preprocess.ops.fmix import sample_mask
 
 class BatchOperator(object):
     """ BatchOperator """
+
     def __init__(self, *args, **kwargs):
         pass
 
@@ -45,35 +46,44 @@ class BatchOperator(object):
 
 
 class MixupOperator(BatchOperator):
-    """ Mixup operator """
-    def __init__(self, alpha=0.2):
-        assert alpha > 0., \
-                'parameter alpha[%f] should > 0.0' % (alpha)
-        self._alpha = alpha
+    """Mixup and Cutmix operator"""
 
-    def __call__(self, batch):
-        imgs, labels, bs = self._unpack(batch)
+    def __init__(self,
+                 mixup_alpha: float=1.,
+                 cutmix_alpha: float=0.,
+                 switch_prob: float=0.5):
+        """Build Mixup operator
+
+        Args:
+            mixup_alpha (float, optional): The parameter alpha of mixup, mixup is active if > 0. Defaults to 1..
+            cutmix_alpha (float, optional): The parameter alpha of cutmix, cutmix is active if > 0. Defaults to 0..
+            switch_prob (float, optional): The probability of switching to cutmix instead of mixup when both are active. Defaults to 0.5.
+
+        Raises:
+            Exception: The value of parameters are illegal.
+        """
+        if mixup_alpha <= 0 and cutmix_alpha <= 0:
+            raise Exception(
+                f"At least one of parameter alpha of Mixup and Cutmix is greater than 0. mixup_alpha: {mixup_alpha}, cutmix_alpha: {cutmix_alpha}"
+            )
+        self._mixup_alpha = mixup_alpha
+        self._cutmix_alpha = cutmix_alpha
+        self._switch_prob = switch_prob
+
+    def _mixup(self, imgs, labels, bs):
         idx = np.random.permutation(bs)
-        lam = np.random.beta(self._alpha, self._alpha)
+        lam = np.random.beta(self._mixup_alpha, self._mixup_alpha)
         lams = np.array([lam] * bs, dtype=np.float32)
         imgs = lam * imgs + (1 - lam) * imgs[idx]
         return list(zip(imgs, labels, labels[idx], lams))
-
-
-class CutmixOperator(BatchOperator):
-    """ Cutmix operator """
-    def __init__(self, alpha=0.2):
-        assert alpha > 0., \
-                'parameter alpha[%f] should > 0.0' % (alpha)
-        self._alpha = alpha
 
     def _rand_bbox(self, size, lam):
         """ _rand_bbox """
         w = size[2]
         h = size[3]
         cut_rat = np.sqrt(1. - lam)
-        cut_w = np.int(w * cut_rat)
-        cut_h = np.int(h * cut_rat)
+        cut_w = int(w * cut_rat)
+        cut_h = int(h * cut_rat)
 
         # uniform
         cx = np.random.randint(w)
@@ -86,10 +96,9 @@ class CutmixOperator(BatchOperator):
 
         return bbx1, bby1, bbx2, bby2
 
-    def __call__(self, batch):
-        imgs, labels, bs = self._unpack(batch)
+    def _cutmix(self, imgs, labels, bs):
         idx = np.random.permutation(bs)
-        lam = np.random.beta(self._alpha, self._alpha)
+        lam = np.random.beta(self._cutmix_alpha, self._cutmix_alpha)
 
         bbx1, bby1, bbx2, bby2 = self._rand_bbox(imgs.shape, lam)
         imgs[:, :, bbx1:bbx2, bby1:bby2] = imgs[idx, :, bbx1:bbx2, bby1:bby2]
@@ -98,9 +107,24 @@ class CutmixOperator(BatchOperator):
         lams = np.array([lam] * bs, dtype=np.float32)
         return list(zip(imgs, labels, labels[idx], lams))
 
+    def __call__(self, batch):
+        imgs, labels, bs = self._unpack(batch)
+        if np.random.rand() < self._switch_prob:
+            return self._cutmix(imgs, labels, bs)
+        else:
+            return self._mixup(imgs, labels, bs)
+
+
+class CutmixOperator(BatchOperator):
+    def __init__(self, **kwargs):
+        raise Exception(
+            f"\"CutmixOperator\" has been deprecated. Please use MixupOperator with \"cutmix_alpha\" and \"switch_prob\" to enable Cutmix. Refor to doc for details."
+        )
+
 
 class FmixOperator(BatchOperator):
     """ Fmix operator """
+
     def __init__(self, alpha=1, decay_power=3, max_soft=0., reformulate=False):
         self._alpha = alpha
         self._decay_power = decay_power
