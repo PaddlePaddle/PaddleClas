687c13522e807af37cfbb07407e814b7a045ec9d
dongshuilong <dongshuilong@baidu.com>
2021-08-26 04:12:26 +0000

    replace vector_search with faiss

--- deploy/python/build_gallery.py
+++ deploy/python/build_gallery.py
@@ -17,13 +17,13 @@ import sys
 __dir__ = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.abspath(os.path.join(__dir__, '../')))
 
-import copy
 import cv2
+import faiss
 import numpy as np
 from tqdm import tqdm
+import pickle
 
 from python.predict_rec import RecPredictor
-from vector_search import Graph_Index
 
 from utils import logger
 from utils import config
@@ -31,9 +31,9 @@ from utils import config
 
 def split_datafile(data_file, image_root, delimiter="\t"):
     '''
-        data_file: image path and info, which can be splitted by spacer 
+        data_file: image path and info, which can be splitted by spacer
         image_root: image path root
-        delimiter: delimiter 
+        delimiter: delimiter
     '''
     gallery_images = []
     gallery_docs = []
@@ -45,9 +45,8 @@ def split_datafile(data_file, image_root, delimiter="\t"):
             assert text_num >= 2, f"line({ori_line}) must be splitted into at least 2 parts, but got {text_num}"
             image_file = os.path.join(image_root, line[0])
 
-            image_doc = line[1]
             gallery_images.append(image_file)
-            gallery_docs.append(image_doc)
+            gallery_docs.append(ori_line.strip())
 
     return gallery_images, gallery_docs
 
@@ -64,9 +63,77 @@ class GalleryBuilder(object):
         '''
             build index from scratch
         '''
+        operation_method = config.get("index_operation", "new").lower()
+
         gallery_images, gallery_docs = split_datafile(
             config['data_file'], config['image_root'], config['delimiter'])
+        if operation_method != "remove":
+            gallery_features = self._extract_features(gallery_images, config)
 
+        assert operation_method in [
+            "new", "remove", "append"
+        ], "Only append, remove and new operation are supported"
+        if operation_method in ["remove", "append"]:
+            assert os.path.join(
+                config["index_dir"], "vector.index"
+            ), "The vector.index dose not exist in {} when 'index_operation' is not None".format(
+                config["index_dir"])
+            assert os.path.join(
+                config["index_dir"], "id_map.pkl"
+            ), "The id_map.pkl dose not exist in {} when 'index_operation' is not None".format(
+                config["index_dir"])
+            index = faiss.read_index(
+                os.path.join(config["index_dir"], "vector.index"))
+            with open(os.path.join(config["index_dir"], "id_map.pkl"),
+                      'rb') as fd:
+                ids = pickle.load(fd)
+            assert index.ntotal == len(ids.keys(
+            )), "data number in index is not equal in in id_map"
+        else:
+            if not os.path.exists(config["index_dir"]):
+                os.makedirs(config["index_dir"], exist_ok=True)
+            index_method = config.get("index_method", "HNSW32")
+            if index_method == "IVF":
+                index_method = index_method + str(
+                    min(int(len(gallery_images) // 8), 65536)) + ",Flat"
+            dist_type = faiss.METRIC_INNER_PRODUCT if config[
+                "dist_type"] == "IP" else faiss.METRIC_L2
+            index = faiss.index_factory(config["embedding_size"], index_method,
+                                        dist_type)
+            index = faiss.IndexIDMap2(index)
+            ids = {}
+
+        if config["index_method"] == "HNSW32":
+            logger.warning(
+                "The HNSW32 method dose not support 'remove' operation")
+
+        if operation_method != "remove":
+            start_id = max(ids.keys()) + 1 if ids else 0
+            ids_now = np.arange(0, len(gallery_images)) + start_id
+            if operation_method == "new":
+                index.train(gallery_features)
+            index.add_with_ids(gallery_features, ids_now)
+
+            for i, d in zip(list(ids_now), gallery_docs):
+                ids[i] = d
+        else:
+            if config["index_method"] == "HNSW32":
+                raise RuntimeError(
+                    "The index_method: HNSW32 dose not support 'remove' operation"
+                )
+            remove_ids = list(
+                filter(lambda k: ids.get(k) in gallery_docs, ids.keys()))
+            remove_ids = np.asarray(remove_ids)
+            index.remove_ids(remove_ids)
+            for k in remove_ids:
+                del ids[k]
+
+        faiss.write_index(index,
+                          os.path.join(config["index_dir"], "vector.index"))
+        with open(os.path.join(config["index_dir"], "id_map.pkl"), 'wb') as fd:
+            pickle.dump(ids, fd)
+
+    def _extract_features(self, gallery_images, config):
         # extract gallery features
         gallery_features = np.zeros(
             [len(gallery_images), config['embedding_size']], dtype=np.float32)
@@ -91,19 +158,11 @@ class GalleryBuilder(object):
             rec_feat = self.rec_predictor.predict(batch_img)
             gallery_features[-len(batch_img):, :] = rec_feat
             batch_img = []
-
-        # train index 
-        self.Searcher = Graph_Index(dist_type=config['dist_type'])
-        self.Searcher.build(
-            gallery_vectors=gallery_features,
-            gallery_docs=gallery_docs,
-            pq_size=config['pq_size'],
-            index_path=config['index_path'],
-            append_index=config["append_index"])
+        return gallery_features
 
 
 def main(config):
-    system_builder = GalleryBuilder(config)
+    GalleryBuilder(config)
     return
 
 
