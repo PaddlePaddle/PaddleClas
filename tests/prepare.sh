3c7f6e4d5c16cc154c41270e28d3960552d8cb46
dongshuilong <dongshuilong@baidu.com>
2021-08-31 12:52:17 +0000

    add cpp whole_chain test

--- tests/prepare.sh
+++ tests/prepare.sh
@@ -33,7 +33,7 @@ if [ ${MODE} = "lite_train_infer" ] || [ ${MODE} = "whole_infer" ];then
     mv train.txt train_list.txt
     mv val.txt val_list.txt
     cd ../../
-elif [ ${MODE} = "infer" ];then
+elif [ ${MODE} = "infer" ] || [ ${MODE} = "cpp_infer" ];then
     # download data
     cd dataset
     rm -rf ILSVRC2012
@@ -57,4 +57,64 @@ elif [ ${MODE} = "whole_train_infer" ];then
     mv train.txt train_list.txt
     mv val.txt val_list.txt
     cd ../../
+fi
+
+if [ ${MODE} = "cpp_infer" ];then
+    cd deploy/cpp
+    echo "################### build opencv ###################"
+    rm -rf 3.4.7.tar.gz opencv-3.4.7/
+    wget https://github.com/opencv/opencv/archive/3.4.7.tar.gz
+    tar -xf 3.4.7.tar.gz
+    install_path=$(pwd)/opencv-3.4.7/opencv3
+    cd opencv-3.4.7/
+
+    rm -rf build
+    mkdir build
+    cd build
+    cmake .. \
+	-DCMAKE_INSTALL_PREFIX=${install_path} \
+	-DCMAKE_BUILD_TYPE=Release \
+	-DBUILD_SHARED_LIBS=OFF \
+	-DWITH_IPP=OFF \
+	-DBUILD_IPP_IW=OFF \
+	-DWITH_LAPACK=OFF \
+	-DWITH_EIGEN=OFF \
+	-DCMAKE_INSTALL_LIBDIR=lib64 \
+	-DWITH_ZLIB=ON \
+	-DBUILD_ZLIB=ON \
+	-DWITH_JPEG=ON \
+	-DBUILD_JPEG=ON \
+	-DWITH_PNG=ON \
+	-DBUILD_PNG=ON \
+	-DWITH_TIFF=ON \
+	-DBUILD_TIFF=ON
+     make -j
+     make install
+     cd ../../
+     echo "################### build opencv finished ###################"
+
+     echo "################### build PaddleClas demo ####################"
+     OPENCV_DIR=$(pwd)/opencv-3.4.7/opencv3/
+     LIB_DIR=$(pwd)/Paddle/build/paddle_inference_install_dir/
+     CUDA_LIB_DIR=$(dirname `find /usr -name libcudart.so`)
+     CUDNN_LIB_DIR=$(dirname `find /usr -name libcudnn.so`)
+
+     BUILD_DIR=build
+     rm -rf ${BUILD_DIR}
+     mkdir ${BUILD_DIR}
+     cd ${BUILD_DIR}
+     cmake .. \
+        -DPADDLE_LIB=${LIB_DIR} \
+        -DWITH_MKL=ON \
+        -DDEMO_NAME=clas_system \
+        -DWITH_GPU=OFF \
+        -DWITH_STATIC_LIB=OFF \
+        -DWITH_TENSORRT=OFF \
+        -DTENSORRT_DIR=${TENSORRT_DIR} \
+        -DOPENCV_DIR=${OPENCV_DIR} \
+        -DCUDNN_LIB=${CUDNN_LIB_DIR} \
+        -DCUDA_LIB=${CUDA_LIB_DIR} \
+
+     make -j
+     echo "################### build PaddleClas demo finished ###################"
 fi
