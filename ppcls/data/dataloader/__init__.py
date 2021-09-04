b6144fb7cf2ae87248a0a19c232e02a513f81b52
weishengyu <weishengyu@baidu.com>
2021-09-04 17:11:23 +0800

    add mix dataloader and mix sampler

---
+++ ppcls/data/dataloader/__init__.py
@@ -0,0 +1,6 @@
+from ppcls.data.dataloader.imagenet_dataset import ImageNetDataset
+from ppcls.data.dataloader.multilabel_dataset import MultiLabelDataset
+from ppcls.data.dataloader.common_dataset import create_operators
+from ppcls.data.dataloader.vehicle_dataset import CompCars, VeriWild
+from ppcls.data.dataloader.logo_dataset import LogoDataset
+from ppcls.data.dataloader.icartoon_dataset import ICartoonDataset
