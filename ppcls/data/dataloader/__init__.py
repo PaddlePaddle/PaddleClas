7830860bbf90c27165930615ca4ce7b330e455ad
weishengyu <weishengyu@baidu.com>
2021-09-18 11:11:48 +0800

    rm writer dataset and sampler

--- ppcls/data/dataloader/__init__.py
+++ ppcls/data/dataloader/__init__.py
@@ -4,7 +4,6 @@ from ppcls.data.dataloader.common_dataset import create_operators
 from ppcls.data.dataloader.vehicle_dataset import CompCars, VeriWild
 from ppcls.data.dataloader.logo_dataset import LogoDataset
 from ppcls.data.dataloader.icartoon_dataset import ICartoonDataset
-from ppcls.data.dataloader.writer_hard_sampler import WriterHardSampler
 from ppcls.data.dataloader.mix_dataset import MixDataset
 from ppcls.data.dataloader.mix_sampler import MixSampler
-from ppcls.data.dataloader.writer_hard_dataset import WriterHardDataset
+from ppcls.data.dataloader.pk_sampler import PKSampler
