f6cfa0471ca8ddf3202effdff70f3895299caedc
weishengyu <weishengyu@baidu.com>
2021-09-18 11:13:54 +0800

    dbg

--- ppcls/data/__init__.py
+++ ppcls/data/__init__.py
@@ -27,11 +27,10 @@ from ppcls.data.dataloader.vehicle_dataset import CompCars, VeriWild
 from ppcls.data.dataloader.logo_dataset import LogoDataset
 from ppcls.data.dataloader.icartoon_dataset import ICartoonDataset
 from ppcls.data.dataloader.mix_dataset import MixDataset
-from ppcls.data.dataloader.writer_hard_dataset import WriterHardDataset
 
 # sampler
 from ppcls.data.dataloader.DistributedRandomIdentitySampler import DistributedRandomIdentitySampler
-from ppcls.data.dataloader.writer_hard_sampler import WriterHardSampler
+from ppcls.data.dataloader.pk_sampler import PKSampler
 from ppcls.data.dataloader.mix_sampler import MixSampler
 from ppcls.data import preprocess
 from ppcls.data.preprocess import transform
