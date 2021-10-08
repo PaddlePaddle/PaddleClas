ed459a2a166ffea31ccbb0a29ca6ad90155d939b
gaotingquan <gaotingquan@baidu.com>
2021-10-08 14:22:41 +0000

    refactor: adapt to static graph in deprecating MixCELoss

--- ppcls/engine/engine.py
+++ ppcls/engine/engine.py
@@ -112,6 +112,7 @@ class Engine(object):
             }
             paddle.fluid.set_flags(AMP_RELATED_FLAGS_SETTING)
 
+        #TODO(gaotingquan): support rec
         class_num = config["Arch"].get("class_num", None)
         self.config["DataLoader"].update({"class_num": class_num})
         # build dataloader
