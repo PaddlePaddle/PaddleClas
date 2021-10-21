fb12983560857c34f055053a87e9d8b45d840390
littletomatodonkey <dazhiningsibuqu@163.com>
2021-10-21 14:46:19 +0800

    fix is_rec

--- ppcls/engine/engine.py
+++ ppcls/engine/engine.py
@@ -54,7 +54,8 @@ class Engine(object):
         self.config = config
         self.eval_mode = self.config["Global"].get("eval_mode",
                                                    "classification")
-        if "Head" in self.config["Arch"]:
+        if "Head" in self.config["Arch"] or self.config["Arch"].get("is_rec",
+                                                                    False):
             self.is_rec = True
         else:
             self.is_rec = False
