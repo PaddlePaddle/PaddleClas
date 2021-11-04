cad5ce842a6468b3ab79a8758fb2886b2e3b150b
Bin Lu <lubin10@baidu.com>
2021-11-04 16:44:16 +0800

    Update __init__.py

--- ppcls/arch/backbone/variant_models/__init__.py
+++ ppcls/arch/backbone/variant_models/__init__.py
@@ -1,3 +1,3 @@
 from .resnet_variant import ResNet50_last_stage_stride1
 from .vgg_variant import VGG19Sigmoid
-from .pp_lcnet_variant import PPLCNet_x2_5Tanh
+from .pp_lcnet_variant import PPLCNet_x2_5_Tanh
