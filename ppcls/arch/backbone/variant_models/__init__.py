6f13c5b79898b8dc84a5bc5c0a7df62e84e8674b
stephon <lubin10@baidu.com>
2021-11-04 08:37:21 +0000

    rename PPLCNet_x2_5_Tanh

--- ppcls/arch/backbone/variant_models/__init__.py
+++ ppcls/arch/backbone/variant_models/__init__.py
@@ -1,3 +1,3 @@
 from .resnet_variant import ResNet50_last_stage_stride1
 from .vgg_variant import VGG19Sigmoid
-from .lcnet_variant import PPLCNet_x2_5Tanh
+from .pp_lcnet_variant import PPLCNet_x2_5Tanh
