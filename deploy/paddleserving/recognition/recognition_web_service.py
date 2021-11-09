eab806a331f2a7b53963c5f38e8c3e8afc4716ae
stephon <lubin10@baidu.com>
2021-11-09 08:38:02 +0000

    modify some code format

--- deploy/paddleserving/recognition/recognition_web_service.py
+++ deploy/paddleserving/recognition/recognition_web_service.py
@@ -64,12 +64,12 @@ class DetOp(Op):
             im_scale_y, im_scale_x = self.generate_scale(raw_im)
             im = self.img_preprocess(raw_im)
 
+            im_shape = np.array(list(im.shape[1:])).reshape(-1)
+            scale_factor = np.array([im_scale_y, im_scale_x]).reshape(-1)
             imgs.append({
                 "image": im[np.newaxis, :],
-                "im_shape":
-                np.array(list(im.shape[1:])).reshape(-1)[np.newaxis, :],
-                "scale_factor":
-                np.array([im_scale_y, im_scale_x]).reshape(-1)[np.newaxis, :],
+                "im_shape": im_shape[np.newaxis, :],
+                "scale_factor": scale_factor[np.newaxis, :],
             })
         self.raw_img = raw_imgs
 
