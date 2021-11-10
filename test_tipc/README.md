79bbc286aaec0ca583bb7b5b2f615529785edf6a
stephon <lubin10@baidu.com>
2021-11-10 11:38:22 +0000

    add test_tipc

---
+++ test_tipc/README.md
@@ -0,0 +1,67 @@
+
+# 飞桨训推一体认证
+
+## 1. 简介
+
+飞桨除了基本的模型训练和预测，还提供了支持多端多平台的高性能推理部署工具。本文档提供了PaddleOCR中所有模型的飞桨训推一体认证 (Training and Inference Pipeline Certification(TIPC)) 信息和测试工具，方便用户查阅每种模型的训练推理部署打通情况，并可以进行一键测试。
+
+<div align="center">
+    <img src="docs/guide.png" width="1000">
+</div>
+
+## 2. 汇总信息
+
+打通情况汇总如下，已填写的部分表示可以使用本工具进行一键测试，未填写的表示正在支持中。
+
+**字段说明：**
+- 基础训练预测：包括模型训练、Paddle Inference Python预测。
+- 更多训练方式：包括多机多卡、混合精度。
+- 模型压缩：包括裁剪、离线/在线量化、蒸馏。
+- 其他预测部署：包括Paddle Inference C++预测、Paddle Serving部署、Paddle-Lite部署等。
+
+更详细的mkldnn、Tensorrt等预测加速相关功能的支持情况可以查看各测试工具的[更多教程](#more)。
+
+## 3. 一键测试工具使用
+### 目录介绍
+./test_tipc/
+├── common_func.sh
+├── config
+│   ├── MobileNetV3_large_x1_0
+│   │   ├── train_infer_python.txt
+│   │   ├── train_linux_gpu_fleet_amp_infer_python_linux_gpu_cpu.txt
+│   │   └── train_linux_gpu_normal_amp_infer_python_linux_gpu_cpu.txt
+│   └── ResNet50_vd
+│       ├── train_infer_python.txt
+│       ├── train_linux_gpu_fleet_amp_infer_python_linux_gpu_cpu.txt
+│       └── train_linux_gpu_normal_amp_infer_python_linux_gpu_cpu.txt
+├── docs
+│   ├── guide.png
+│   └── test.png
+├── prepare.sh
+├── README.md
+├── results
+└── test_train_inference_python.sh
+
+### 测试流程
+使用本工具，可以测试不同功能的支持情况，以及预测结果是否对齐，测试流程如下：
+<div align="center">
+    <img src="docs/test.png" width="800">
+</div>
+
+1. 运行prepare.sh准备测试所需数据和模型；
+2. 运行要测试的功能对应的测试脚本`test_*.sh`，产出log，由log可以看到不同配置是否运行成功；
+3. 用`compare_results.py`对比log中的预测结果和预存在results目录下的结果，判断预测精度是否符合预期（在误差范围内）。
+
+其中，有4个测试主程序，功能如下：
+- `test_train_inference_python.sh`：测试基于Python的模型训练、评估、推理等基本功能，包括裁剪、量化、蒸馏。
+- `test_inference_cpp.sh`：测试基于C++的模型推理。
+- `test_serving.sh`：测试基于Paddle Serving的服务化部署功能。
+- `test_lite.sh`：测试基于Paddle-Lite的端侧预测部署功能。
+
+<a name="more"></a>
+#### 更多教程
+各功能测试中涉及混合精度、裁剪、量化等训练相关，及mkldnn、Tensorrt等多种预测相关参数配置，请点击下方相应链接了解更多细节和使用教程：  
+[test_train_inference_python 使用](docs/test_train_inference_python.md)  
+[test_inference_cpp 使用](docs/test_inference_cpp.md)  
+[test_serving 使用](docs/test_serving.md)  
+[test_lite 使用](docs/test_lite.md)
---
+++ test_tipc/prepare.sh
@@ -0,0 +1,153 @@
+#!/bin/bash
+FILENAME=$1
+
+# MODE be one of ['lite_train_lite_infer' 'lite_train_whole_infer' 'whole_train_whole_infer',  
+#                 'whole_infer', 'klquant_whole_infer',
+#                 'cpp_infer', 'serving_infer',  'lite_infer']
+
+MODE=$2
+
+dataline=$(cat ${FILENAME})
+# parser params
+IFS=$'\n'
+lines=(${dataline})
+
+function func_parser_key(){
+    strs=$1
+    IFS=":"
+    array=(${strs})
+    tmp=${array[0]}
+    echo ${tmp}
+}
+
+function func_parser_value(){
+    strs=$1
+    IFS=":"
+    array=(${strs})
+    if [ ${#array[*]} = 2 ]; then
+        echo ${array[1]}
+    else
+    	IFS="|"
+    	tmp="${array[1]}:${array[2]}"
+        echo ${tmp}
+    fi
+}
+
+model_name=$(func_parser_value "${lines[1]}")
+model_url_value=$(func_parser_value "${lines[35]}")
+model_url_key=$(func_parser_key "${lines[35]}")
+
+if [ ${MODE} = "lite_train_lite_infer" ] || [ ${MODE} = "lite_train_whole_infer" ];then
+    # pretrain lite train data
+    cd dataset
+    rm -rf ILSVRC2012
+    wget -nc https://paddle-imagenet-models-name.bj.bcebos.com/data/whole_chain/whole_chain_little_train.tar
+    tar xf whole_chain_little_train.tar
+    ln -s whole_chain_little_train ILSVRC2012
+    cd ILSVRC2012 
+    mv train.txt train_list.txt
+    mv val.txt val_list.txt
+    if [ ${MODE} = "lite_train_lite_infer" ];then
+	cp -r train/* val/
+    fi
+    cd ../../
+elif [ ${MODE} = "whole_infer" ] || [ ${MODE} = "cpp_infer" ];then
+    # download data
+    cd dataset
+    rm -rf ILSVRC2012
+    wget -nc https://paddle-imagenet-models-name.bj.bcebos.com/data/whole_chain/whole_chain_infer.tar
+    tar xf whole_chain_infer.tar
+    ln -s whole_chain_infer ILSVRC2012
+    cd ILSVRC2012 
+    mv val.txt val_list.txt
+    ln -s val_list.txt train_list.txt
+    cd ../../
+    # download inference or pretrained model
+    eval "wget -nc $model_url_value"
+    if [[ $model_url_key == *inference* ]]; then
+	rm -rf inference
+	tar xf "${model_name}_inference.tar"
+    fi
+elif [ ${MODE} = "whole_train_whole_infer" ];then
+    cd dataset
+    rm -rf ILSVRC2012
+    wget -nc https://paddle-imagenet-models-name.bj.bcebos.com/data/whole_chain/whole_chain_CIFAR100.tar
+    tar xf whole_chain_CIFAR100.tar
+    ln -s whole_chain_CIFAR100 ILSVRC2012
+    cd ILSVRC2012 
+    mv train.txt train_list.txt
+    mv val.txt val_list.txt
+    cd ../../
+fi
+
+if [ ${MODE} = "serving_infer" ];then
+    # prepare serving env
+    python_name=$(func_parser_value "${lines[2]}")
+    ${python_name} -m pip install install paddle-serving-server-gpu==0.6.1.post101
+    ${python_name} -m pip install paddle_serving_client==0.6.1
+    ${python_name} -m pip install paddle-serving-app==0.6.1
+    unset http_proxy
+    unset https_proxy
+    cd ./deploy/paddleserving
+    wget -nc https://paddle-imagenet-models-name.bj.bcebos.com/dygraph/inference/ResNet50_vd_infer.tar && tar xf ResNet50_vd_infer.tar
+fi
+
+if [ ${MODE} = "cpp_infer" ];then
+    cd deploy/cpp
+    echo "################### build opencv ###################"
+    rm -rf 3.4.7.tar.gz opencv-3.4.7/
+    wget https://github.com/opencv/opencv/archive/3.4.7.tar.gz
+    tar -xf 3.4.7.tar.gz
+    install_path=$(pwd)/opencv-3.4.7/opencv3
+    cd opencv-3.4.7/
+
+    rm -rf build
+    mkdir build
+    cd build
+    cmake .. \
+	-DCMAKE_INSTALL_PREFIX=${install_path} \
+	-DCMAKE_BUILD_TYPE=Release \
+	-DBUILD_SHARED_LIBS=OFF \
+	-DWITH_IPP=OFF \
+	-DBUILD_IPP_IW=OFF \
+	-DWITH_LAPACK=OFF \
+	-DWITH_EIGEN=OFF \
+	-DCMAKE_INSTALL_LIBDIR=lib64 \
+	-DWITH_ZLIB=ON \
+	-DBUILD_ZLIB=ON \
+	-DWITH_JPEG=ON \
+	-DBUILD_JPEG=ON \
+	-DWITH_PNG=ON \
+	-DBUILD_PNG=ON \
+	-DWITH_TIFF=ON \
+	-DBUILD_TIFF=ON
+     make -j
+     make install
+     cd ../../
+     echo "################### build opencv finished ###################"
+
+     echo "################### build PaddleClas demo ####################"
+     OPENCV_DIR=$(pwd)/opencv-3.4.7/opencv3/
+     LIB_DIR=$(pwd)/Paddle/build/paddle_inference_install_dir/
+     CUDA_LIB_DIR=$(dirname `find /usr -name libcudart.so`)
+     CUDNN_LIB_DIR=$(dirname `find /usr -name libcudnn.so`)
+
+     BUILD_DIR=build
+     rm -rf ${BUILD_DIR}
+     mkdir ${BUILD_DIR}
+     cd ${BUILD_DIR}
+     cmake .. \
+        -DPADDLE_LIB=${LIB_DIR} \
+        -DWITH_MKL=ON \
+        -DDEMO_NAME=clas_system \
+        -DWITH_GPU=OFF \
+        -DWITH_STATIC_LIB=OFF \
+        -DWITH_TENSORRT=OFF \
+        -DTENSORRT_DIR=${TENSORRT_DIR} \
+        -DOPENCV_DIR=${OPENCV_DIR} \
+        -DCUDNN_LIB=${CUDNN_LIB_DIR} \
+        -DCUDA_LIB=${CUDA_LIB_DIR} \
+
+     make -j
+     echo "################### build PaddleClas demo finished ###################"
+fi
