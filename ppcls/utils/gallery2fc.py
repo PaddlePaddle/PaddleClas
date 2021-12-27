aafa8f329d8285b258af22ec322a35f8b9da3350
weishengyu <weishengyu@baidu.com>
2021-12-27 20:16:56 +0800

    add license info

--- ppcls/utils/gallery2fc.py
+++ ppcls/utils/gallery2fc.py
@@ -1,13 +1,26 @@
+# Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import os
 import paddle
 import cv2
 
 from ppcls.arch import build_model
-from ppcls.arch.gears.identity_head import IdentityHead
 from ppcls.utils.config import parse_config, parse_args
 from ppcls.utils.save_load import load_dygraph_pretrain
 from ppcls.utils.logger import init_logger
-from ppcls.data import transform, create_operators
+from ppcls.data import create_operators
 from ppcls.arch.slim import quantize_model
 
 
