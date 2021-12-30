PaddlePaddle/PaddleClas

commit 5d53e9f15207f3bd68d74e85e5595b78fe9b6590

fix: raise warning when setting batch_transform_ops and TopkAcc

ppcls/engine/engine.py
@@ -161,6 +161,13 @@ class Engine(object):
             if metric_config is not None:
                 metric_config = metric_config.get("Train")
                 if metric_config is not None:
+                    if self.train_dataloader.collate_fn:
+                        for m_idx, m in enumerate(metric_config):
+                            if "TopkAcc" in m:
+                                msg = f"'TopkAcc' metric can not be used when setting 'batch_transform_ops' in config. The 'TopkAcc' metric has been removed."
+                                logger.warning(msg)
+                                break
+                        metric_config.pop(m_idx)
                     self.train_metric_func = build_metrics(metric_config)
                 else:
                     self.train_metric_func = None

commit cbe944e699396900700b9357758e2f121ce060aa

fix: use hasattr() to check if collate_fn is in dataloader
fix bug caused by PR #1596

ppcls/engine/engine.py
@@ -161,7 +161,7 @@ class Engine(object):
             if metric_config is not None:
                 metric_config = metric_config.get("Train")
                 if metric_config is not None:
-                    if self.train_dataloader.collate_fn:
+                    if hasattr(self.train_dataloader, "collate_fn"):
                         for m_idx, m in enumerate(metric_config):
                             if "TopkAcc" in m:
                                 msg = f"'TopkAcc' metric can not be used when setting 'batch_transform_ops' in config. The 'TopkAcc' metric has been removed."