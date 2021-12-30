d6aeba6f5690c608d5466608519e78c8921af580
gaotingquan <gaotingquan@baidu.com>
2021-12-30 08:10:42 +0000

    fix: upgrade replace_sub() to upgrade_sublayer()
    supplement to PR #1581

--- ppcls/arch/backbone/variant_models/pp_lcnet_variant.py
+++ ppcls/arch/backbone/variant_models/pp_lcnet_variant.py
@@ -19,11 +19,11 @@ class TanhSuffix(paddle.nn.Layer):
 
 
 def PPLCNet_x2_5_Tanh(pretrained=False, use_ssld=False, **kwargs):
-    def replace_function(origin_layer):
+    def replace_function(origin_layer, pattern):
         new_layer = TanhSuffix(origin_layer)
         return new_layer
 
-    match_re = "linear_0"
+    pattern = "fc"
     model = PPLCNet_x2_5(pretrained=pretrained, use_ssld=use_ssld, **kwargs)
-    model.replace_sub(match_re, replace_function, True)
+    model.upgrade_sublayer(pattern, replace_function)
     return model
