eee58c3a4a9b2e886ed0232d134cd48c287d3ee4
mmglove <gmm_email@163.com>
2022-03-09 14:44:52 +0800

    fix benchmark

--- test_tipc/static/ResNet50/benchmark_common/run_benchmark.sh
+++ test_tipc/static/ResNet50/benchmark_common/run_benchmark.sh
@@ -1,23 +1,22 @@
 #!/usr/bin/env bash
 # Test training benchmark for a model.
-# Usage：bash run_benchmark.sh ${model_item} ${bs_item} ${fp_item} ${run_process_type} ${run_mode} ${device_num}
+# Usage：bash run_benchmark.sh ${model_item} ${bs_item} ${fp_item} ${run_mode} ${device_num}
 function _set_params(){
     model_item=${1:-"model_item"}   # (必选) 模型 item
     base_batch_size=${2:-"2"}       # (必选) 如果是静态图单进程，则表示每张卡上的BS，需在训练时*卡数
     fp_item=${3:-"fp32"}            # (必选) fp32|fp16
-    run_process_type=${4:-"SingleP"} # (必选) 单进程 SingleP|多进程 MultiP
-    run_mode=${5:-"DP"}             # (必选) MP模型并行|DP数据并行|PP流水线并行|混合并行DP1-MP1-PP1|DP1-MP4-PP1
-    device_num=${6:-"N1C1"}         # (必选) 使用的卡数量，N1C1|N1C8|N4C32 （4机32卡）
+    run_mode=${4:-"DP"}             # (必选) MP模型并行|DP数据并行|PP流水线并行|混合并行DP1-MP1-PP1|DP1-MP4-PP1
+    device_num=${5:-"N1C1"}         # (必选) 使用的卡数量，N1C1|N1C8|N4C32 （4机32卡）
     profiling=${PROFILING:-"false"}      # (必选) Profiling  开关，默认关闭，通过全局变量传递
     model_repo="PaddleClas"          # (必选) 模型套件的名字
     speed_unit="samples/sec"         # (必选)速度指标单位
     skip_steps=10                  # (必选)解析日志，跳过模型前几个性能不稳定的step
     keyword="ips:"                 # (必选)解析日志，筛选出性能数据所在行的关键字
     convergence_key="loss:"        # (可选)解析日志，筛选出收敛数据所在行的关键字 如：convergence_key="loss:"
-    max_epochs=${7:-"1"}           # （可选）需保证模型执行时间在5分钟内，需要修改代码提前中断的直接提PR 合入套件；或使用max_epoch参数
-    num_workers=${8:-"4"}                  # (可选)
+    max_epochs=${6:-"1"}           # （可选）需保证模型执行时间在5分钟内，需要修改代码提前中断的直接提PR 合入套件；或使用max_epoch参数
+    num_workers=${7:-"4"}                  # (可选)
 #   以下为通用执行命令，无特殊可不用修改
-    model_name=${model_item}_bs${base_batch_size}_${fp_item}_${run_process_type}_${run_mode}  # (必填) 且格式不要改动,与竞品名称对齐
+    model_name=${model_item}_bs${base_batch_size}_${fp_item}_${run_mode}  # (必填) 且格式不要改动,与竞品名称对齐
     device=${CUDA_VISIBLE_DEVICES//,/ }
     arr=(${device})
     num_gpu_devices=${#arr[*]}
@@ -48,13 +47,19 @@ function _train(){
 
     train_cmd="${config_file} -o DataLoader.Train.sampler.batch_size=${base_batch_size} -o Global.epochs=${max_epochs} -o DataLoader.Train.loader.num_workers=${num_workers} ${profiling_config} -o Global.eval_during_train=False"
 #   以下为通用执行命令，无特殊可不用修改
-    case ${run_process_type} in
-    SingleP) 
-	   train_cmd="python ppcls/static/train.py ${train_cmd}";;
-    MultiP)
-	   train_cmd="python -m paddle.distributed.launch --gpus 0,1,2,3,4,5,6,7 ppcls/static/train.py ${train_cmd}";;
-    *) echo "choose run_process_type(SingleP or MultiP)"; exit 1;
+    case ${run_mode} in
+    DP) if [[ ${device_num} = "N1C1" ]];then
+            echo "run ${run_mode} ${device_num}"
+	        train_cmd="python ppcls/static/train.py ${train_cmd}"
+        else
+            rm -rf ./mylog
+            train_cmd="python -m paddle.distributed.launch --gpus 0,1,2,3,4,5,6,7 ppcls/static/train.py ${train_cmd}"
+        fi
+        ;;
+    DP1-MP1-PP1)  echo "run run_mode: DP1-MP1-PP1" ;;
+    *) echo "choose run_mode "; exit 1;
     esac
+
     echo "train_cmd: ${train_cmd}  log_file: ${log_file}"
     timeout 5m ${train_cmd} > ${log_file} 2>&1
     if [ $? -ne 0 ];then
@@ -63,7 +68,7 @@ function _train(){
         echo -e "${model_name}, SUCCESS"
     fi
     # kill -9 `ps -ef|grep 'python'|awk '{print $2}'`
-    if [ ${run_process_type} = "MultiP" -a -d mylog ]; then
+    if [ ${device_num} != "N1C1" -a -d mylog ]; then
         rm ${log_file}
         cp mylog/workerlog.0 ${log_file}
     fi
