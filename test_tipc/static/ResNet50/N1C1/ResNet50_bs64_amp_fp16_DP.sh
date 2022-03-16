64033b36deaee6c862432ae8adca00968c903386
dongshuilong <dongshuilong@baidu.com>
2022-03-16 03:09:54 +0000

    delete run_process_type for static benchmark

--- test_tipc/static/ResNet50/N1C1/ResNet50_bs64_amp_fp16_DP.sh
+++ test_tipc/static/ResNet50/N1C1/ResNet50_bs64_amp_fp16_DP.sh
@@ -1,7 +1,6 @@
 model_item=ResNet50
 bs_item=64
 fp_item=amp_fp16
-run_process_type=SingleP
 run_mode=DP
 device_num=N1C1
 max_epochs=1
@@ -10,4 +9,4 @@ num_workers=8
 # get data
 bash test_tipc/static/${model_item}/benchmark_common/prepare.sh
 # run
-bash test_tipc/static/${model_item}/benchmark_common/run_benchmark.sh ${model_item} ${bs_item} ${fp_item} ${run_process_type} ${run_mode} ${device_num} ${max_epochs} ${num_workers} 2>&1;
+bash test_tipc/static/${model_item}/benchmark_common/run_benchmark.sh ${model_item} ${bs_item} ${fp_item} ${run_mode} ${device_num} ${max_epochs} ${num_workers} 2>&1;
--- test_tipc/static/ResNet50/N4C32/ResNet50_bs256_pure_fp16_DP.sh
+++ test_tipc/static/ResNet50/N4C32/ResNet50_bs256_pure_fp16_DP.sh
@@ -1,7 +1,6 @@
 model_item=ResNet50
 bs_item=256
 fp_item=pure_fp16
-run_process_type=MultiP
 run_mode=DP
 device_num=N4C32
 max_epochs=32
@@ -10,4 +9,4 @@ num_workers=8
 # get data
 bash test_tipc/static/${model_item}/benchmark_common/prepare.sh
 # run
-bash test_tipc/static/${model_item}/benchmark_common/run_benchmark.sh ${model_item} ${bs_item} ${fp_item} ${run_process_type} ${run_mode} ${device_num} ${max_epochs} ${num_workers} 2>&1;
+bash test_tipc/static/${model_item}/benchmark_common/run_benchmark.sh ${model_item} ${bs_item} ${fp_item} ${run_mode} ${device_num} ${max_epochs} ${num_workers} 2>&1;
