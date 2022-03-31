dbb8df64d549668d1e4d997a40457790d29df573
weishengyu <weisy11@163.com>
2022-03-31 15:06:06 +0800

    add reid config

--- ppcls/data/preprocess/ops/operators.py
+++ ppcls/data/preprocess/ops/operators.py
@@ -25,6 +25,7 @@ import cv2
 import numpy as np
 from PIL import Image
 from paddle.vision.transforms import ColorJitter as RawColorJitter
+from paddle.vision.transforms import Pad
 
 from .autoaugment import ImageNetPolicy
 from .functional import augmentations
