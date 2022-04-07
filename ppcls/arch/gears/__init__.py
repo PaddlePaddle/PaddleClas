9c99e7cc6b75c886c1c0c10c726248495321e006
weishengyu <weisy11@163.com>
2022-04-07 16:59:09 +0800

    add bnneck, last stride=1

--- ppcls/arch/gears/__init__.py
+++ ppcls/arch/gears/__init__.py
@@ -18,13 +18,14 @@ from .circlemargin import CircleMargin
 from .fc import FC
 from .vehicle_neck import VehicleNeck
 from paddle.nn import Tanh
+from .bnneck import BNNeck
 
 __all__ = ['build_gear']
 
 
 def build_gear(config):
     support_dict = [
-        'ArcMargin', 'CosMargin', 'CircleMargin', 'FC', 'VehicleNeck', 'Tanh'
+        'ArcMargin', 'CosMargin', 'CircleMargin', 'FC', 'VehicleNeck', 'Tanh', "BNNeck"
     ]
     module_name = config.pop('name')
     assert module_name in support_dict, Exception(
---
+++ ppcls/arch/gears/bnneck.py
@@ -0,0 +1,14 @@
+class BNNeck(nn.Layer):
+    def __init__(self, num_filters, trainable=False):
+        super(BNNeck, self).__init__()
+        self.num_filters = num_filters
+
+        self.bn = paddle.nn.BatchNorm(
+            self.num_filters)
+        if not trainable:
+            self.bn.weight.trainable = False
+            self.bn.bias.trainable = False
+
+    def forward(self, input, label=None):
+        out = self.bn(input)
+        return out
