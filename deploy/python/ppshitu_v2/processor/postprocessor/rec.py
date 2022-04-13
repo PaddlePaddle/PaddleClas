c1d0634f6d3753c7e07d07b3d6244da521408703
weishengyu <weisy11@163.com>
2022-04-13 23:43:41 +0800

    dbg

--- deploy/python/ppshitu_v2/processor/postprocessor/rec.py
+++ deploy/python/ppshitu_v2/processor/postprocessor/rec.py
@@ -6,11 +6,15 @@ from processor.base_processor import BaseProcessor
 class FeatureNormalizer(BaseProcessor):
     def __init__(self, config=None):
         super().__init__(config)
+        if self.input_keys is None:
+            self.input_keys = ["features"]
+        if self.output_keys is None:
+            self.output_keys = ["features"]
 
-    def process(self, data):
-        batch_output = data["pred"]["features"]
+    def process(self, input_data):
+        batch_output = input_data[self.input_keys[0]]
         feas_norm = np.sqrt(
             np.sum(np.square(batch_output), axis=1, keepdims=True))
         batch_output = np.divide(batch_output, feas_norm)
-        data["pred"]["features"] = batch_output
-        return data
+        input_data[self.output_keys[0]] = batch_output
+        return input_data
