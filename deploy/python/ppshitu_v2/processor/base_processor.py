7f8d3cac8f77e73bf5dc8bfcc39f5a9af18b4a7c
weishengyu <weisy11@163.com>
2022-04-13 22:56:41 +0800

    dbg

--- deploy/python/ppshitu_v2/processor/base_processor.py
+++ deploy/python/ppshitu_v2/processor/base_processor.py
@@ -4,8 +4,12 @@ from abc import ABC, abstractmethod
 class BaseProcessor(ABC):
     @abstractmethod
     def __init__(self, config):
-        self.input_keys = config.get("input_keys", None)
-        self.output_keys = config.get("output_keys", None)
+        if config is None:
+            self.input_keys = None
+            self.output_keys = None
+        else:
+            self.input_keys = config.get("input_keys", None)
+            self.output_keys = config.get("output_keys", None)
 
     @abstractmethod
     def process(self, input_data):
