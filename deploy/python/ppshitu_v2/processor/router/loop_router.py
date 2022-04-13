3fc6ad253e32cd878b09dd76ac1308310acdb892
weishengyu <weisy11@163.com>
2022-04-13 23:55:14 +0800

    dbg

--- deploy/python/ppshitu_v2/processor/router/loop_router.py
+++ deploy/python/ppshitu_v2/processor/router/loop_router.py
@@ -16,7 +16,7 @@ class LoopRouter(BaseProcessor):
                 processor.process(input_data_i)
             for k in self.output_keys:
                 if k not in input_data:
-                    input_data[k] = input_data_i[k]
+                    input_data[k] = [input_data_i[k]]
                 else:
                     input_data[k].append(input_data_i[k])
         return input_data
