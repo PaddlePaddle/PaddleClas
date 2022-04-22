15997ea9d08e500917427a96bcb837bbc2323093
HydrogenSulfate <490868991@qq.com>
2022-04-22 12:03:17 +0800

    update code

--- ppcls/engine/train/utils.py
+++ ppcls/engine/train/utils.py
@@ -64,7 +64,7 @@ def log_info(trainer, batch_size, epoch_id, iter_id):
 
     for i, lr in enumerate(trainer.lr_sch):
         logger.scaler(
-            name="lr_{}".format(i + 1),
+            name="lr({})".format(lr.__class__.__name__),
             value=lr.get_lr(),
             step=trainer.global_step,
             writer=trainer.vdl_writer)
