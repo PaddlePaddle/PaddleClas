9399748381db4e3978b0eaec3a6a7f57f556937f
HydrogenSulfate <490868991@qq.com>
2022-05-11 14:56:08 +0800

    fix bug in DetPredictor.predict

--- ppcls/engine/evaluation/retrieval.py
+++ ppcls/engine/evaluation/retrieval.py
@@ -14,6 +14,8 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
+from __future__ import division
+from __future__ import print_function
 
 import platform
 import paddle
