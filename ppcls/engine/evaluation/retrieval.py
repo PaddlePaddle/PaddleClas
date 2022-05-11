5f9313698ea3c2d137b66b4f6679d074825885d2
HydrogenSulfate <490868991@qq.com>
2022-05-11 16:33:10 +0800

    Update retrieval.py
    删除冗余import

--- ppcls/engine/evaluation/retrieval.py
+++ ppcls/engine/evaluation/retrieval.py
@@ -14,8 +14,6 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
-from __future__ import division
-from __future__ import print_function
 
 import platform
 import paddle
