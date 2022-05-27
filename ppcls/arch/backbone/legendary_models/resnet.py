7f0075e8d03ffb2678dba26306da7723e5010477
zhiboniu <zhiboniu@163.com>
2022-05-27 07:54:26 +0000

    reverse to BatchNorm

--- ppcls/arch/backbone/legendary_models/resnet.py
+++ ppcls/arch/backbone/legendary_models/resnet.py
@@ -137,11 +137,11 @@ class ConvBNLayer(TheseusLayer):
         weight_attr = ParamAttr(learning_rate=lr_mult, trainable=True)
         bias_attr = ParamAttr(learning_rate=lr_mult, trainable=True)
 
-        self.bn = BatchNorm2D(
+        self.bn = BatchNorm(
             num_filters,
-            weight_attr=ParamAttr(learning_rate=lr_mult),
+            param_attr=ParamAttr(learning_rate=lr_mult),
             bias_attr=ParamAttr(learning_rate=lr_mult),
-            data_format=data_format)
+            data_layout=data_format)
         self.relu = nn.ReLU()
 
     def forward(self, x):
