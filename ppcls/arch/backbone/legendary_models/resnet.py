5d9d2395e27edd5a13701f1a7466c87b20e4f07a
cuicheng01 <cuicheng_smile@163.com>
2022-05-31 10:55:57 +0000

    update resnet&pp-lcnet

--- ppcls/arch/backbone/legendary_models/resnet.py
+++ ppcls/arch/backbone/legendary_models/resnet.py
@@ -20,9 +20,10 @@ import numpy as np
 import paddle
 from paddle import ParamAttr
 import paddle.nn as nn
-from paddle.nn import Conv2D, BatchNorm, Linear
+from paddle.nn import Conv2D, BatchNorm, Linear, BatchNorm2D
 from paddle.nn import AdaptiveAvgPool2D, MaxPool2D, AvgPool2D
 from paddle.nn.initializer import Uniform
+from paddle.regularizer import L2Decay
 import math
 
 from ppcls.arch.backbone.base.theseus_layer import TheseusLayer
@@ -121,17 +122,21 @@ class ConvBNLayer(TheseusLayer):
         self.is_vd_mode = is_vd_mode
         self.act = act
         self.avg_pool = AvgPool2D(
-            kernel_size=2, stride=2, padding=0, ceil_mode=True)
+            kernel_size=2, stride=stride, padding="SAME", ceil_mode=True)
         self.conv = Conv2D(
             in_channels=num_channels,
             out_channels=num_filters,
             kernel_size=filter_size,
-            stride=stride,
+            stride=1 if is_vd_mode else stride,
             padding=(filter_size - 1) // 2,
             groups=groups,
             weight_attr=ParamAttr(learning_rate=lr_mult),
             bias_attr=False,
             data_format=data_format)
+
+        weight_attr = ParamAttr(learning_rate=lr_mult, trainable=True)
+        bias_attr = ParamAttr(learning_rate=lr_mult, trainable=True)
+
         self.bn = BatchNorm(
             num_filters,
             param_attr=ParamAttr(learning_rate=lr_mult),
@@ -159,7 +164,6 @@ class BottleneckBlock(TheseusLayer):
                  lr_mult=1.0,
                  data_format="NCHW"):
         super().__init__()
-
         self.conv0 = ConvBNLayer(
             num_channels=num_channels,
             num_filters=num_filters,
@@ -188,10 +192,11 @@ class BottleneckBlock(TheseusLayer):
                 num_channels=num_channels,
                 num_filters=num_filters * 4,
                 filter_size=1,
-                stride=stride if if_first else 1,
+                stride=stride,
                 is_vd_mode=False if if_first else True,
                 lr_mult=lr_mult,
                 data_format=data_format)
+
         self.relu = nn.ReLU()
         self.shortcut = shortcut
 
@@ -242,7 +247,7 @@ class BasicBlock(TheseusLayer):
                 num_channels=num_channels,
                 num_filters=num_filters,
                 filter_size=1,
-                stride=stride if if_first else 1,
+                stride=stride,
                 is_vd_mode=False if if_first else True,
                 lr_mult=lr_mult,
                 data_format=data_format)
@@ -281,14 +286,17 @@ class ResNet(TheseusLayer):
                  stem_act="relu",
                  class_num=1000,
                  lr_mult_list=[1.0, 1.0, 1.0, 1.0, 1.0],
+                 stride_list=[2, 2, 2, 2, 2],
                  data_format="NCHW",
                  input_image_channel=3,
                  return_patterns=None,
-                 return_stages=None):
+                 return_stages=None,
+                 **kargs):
         super().__init__()
 
         self.cfg = config
         self.lr_mult_list = lr_mult_list
+        self.stride_list = stride_list
         self.is_vd_mode = version == "vd"
         self.class_num = class_num
         self.num_filters = [64, 128, 256, 512]
@@ -305,14 +313,22 @@ class ResNet(TheseusLayer):
                    ) == 5, "lr_mult_list length should be 5 but got {}".format(
                        len(self.lr_mult_list))
 
+        assert isinstance(self.stride_list, (
+            list, tuple
+        )), "stride_list should be in (list, tuple) but got {}".format(
+            type(self.stride_list))
+        assert len(self.stride_list
+                   ) == 5, "stride_list length should be 5 but got {}".format(
+                       len(self.stride_list))
+
         self.stem_cfg = {
             #num_channels, num_filters, filter_size, stride
-            "vb": [[input_image_channel, 64, 7, 2]],
+            "vb": [[input_image_channel, 64, 7, self.stride_list[0]]],
             "vd":
-            [[input_image_channel, 32, 3, 2], [32, 32, 3, 1], [32, 64, 3, 1]]
+            [[input_image_channel, 32, 3, self.stride_list[0]], [32, 32, 3, 1], [32, 64, 3, 1]]
         }
 
-        self.stem = nn.Sequential(*[
+        self.stem = nn.Sequential(* [
             ConvBNLayer(
                 num_channels=in_c,
                 num_filters=out_c,
@@ -325,7 +341,7 @@ class ResNet(TheseusLayer):
         ])
 
         self.max_pool = MaxPool2D(
-            kernel_size=3, stride=2, padding=1, data_format=data_format)
+            kernel_size=3, stride=stride_list[1], padding=1, data_format=data_format)
         block_list = []
         for block_idx in range(len(self.block_depth)):
             shortcut = False
@@ -334,7 +350,7 @@ class ResNet(TheseusLayer):
                     num_channels=self.num_channels[block_idx] if i == 0 else
                     self.num_filters[block_idx] * self.channels_mult,
                     num_filters=self.num_filters[block_idx],
-                    stride=2 if i == 0 and block_idx != 0 else 1,
+                    stride=self.stride_list[block_idx+1] if i == 0 and block_idx != 0 else 1,
                     shortcut=shortcut,
                     if_first=block_idx == i == 0 if version == "vd" else True,
                     lr_mult=self.lr_mult_list[block_idx + 1],
