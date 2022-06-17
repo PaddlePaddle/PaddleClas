31a736f7e70ed610acb2cd43c3038300f2d77037
HydrogenSulfate <490868991@qq.com>
2022-06-17 20:02:24 +0800

    move faiss installtion to prepare.sh

--- test_tipc/prepare.sh
+++ test_tipc/prepare.sh
@@ -209,6 +209,7 @@ if [[ ${MODE} = "serving_infer" ]]; then
     # prepare serving env
     python_name=$(func_parser_value "${lines[2]}")
     if [[ ${model_name} =~ "ShiTu" ]]; then
+        ${python_name} -m pip install faiss-cpu==1.7.1post2 -i https://pypi.tuna.tsinghua.edu.cn/simple
         cls_inference_model_url=$(func_parser_value "${lines[3]}")
         cls_tar_name=$(func_get_url_file_name "${cls_inference_model_url}")
         det_inference_model_url=$(func_parser_value "${lines[4]}")
