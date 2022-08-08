b574a47d6e19c794fac0745db8f4ee1129d4273a
HydrogenSulfate <490868991@qq.com>
2022-08-08 23:02:05 +0800

    update bash scripts and related python file to develop version

--- deploy/paddleserving/run_cpp_serving.sh
+++ deploy/paddleserving/run_cpp_serving.sh
@@ -1,2 +1,14 @@
-#run cls server:
-nohup python3 -m paddle_serving_server.serve --model ResNet50_vd_serving --port 9292 &
+gpu_id=$1
+
+# ResNet50_vd CPP serving script
+if [[ -n "${gpu_id}" ]]; then
+    nohup python3.7 -m paddle_serving_server.serve \
+    --model ./ResNet50_vd_serving \
+    --op GeneralClasOp \
+    --port 9292 &
+else
+    nohup python3.7 -m paddle_serving_server.serve \
+    --model ./ResNet50_vd_serving \
+    --op GeneralClasOp \
+    --port 9292 --gpu_id="${gpu_id}" &
+fi
