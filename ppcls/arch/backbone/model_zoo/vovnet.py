f10720e820aa0e2979cfe4f068a1b5876bd1d4cf
xly <xgboost@qq.com>
2022-09-05 13:45:56 +0800

    Add vovnet

---
+++ ppcls/arch/backbone/model_zoo/vovnet.py
@@ -0,0 +1,237 @@
+# copyright (c) 2022 PaddlePaddle Authors. All Rights Reserve.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import paddle
+import paddle.nn as nn
+import paddle.nn.functional as F
+from paddle.nn.initializer import TruncatedNormal, Constant, Normal, KaimingNormal
+from ....utils.save_load import load_dygraph_pretrain, load_dygraph_pretrain_from_url
+
+trunc_normal_ = TruncatedNormal(std=.02)
+normal_ = Normal
+zeros_ = Constant(value=0.)
+ones_ = Constant(value=1.)
+kaiming_normal_ = KaimingNormal()
+
+
+def conv3x3(in_channels, out_channels, module_name, postfix,
+            stride=1, groups=1, kernel_size=3, padding=1):
+    """3x3 convolution with padding"""
+    return [
+        ('{}_{}/conv'.format(module_name, postfix),
+         nn.Conv2D(in_channels, out_channels,
+                   kernel_size=kernel_size,
+                   stride=stride,
+                   padding=padding,
+                   groups=groups,
+                   bias_attr=False)),
+        ('{}_{}/norm'.format(module_name, postfix),
+         nn.BatchNorm2D(out_channels)),
+        ('{}_{}/relu'.format(module_name, postfix),
+         nn.ReLU()),
+    ]
+
+
+def conv1x1(in_channels, out_channels, module_name, postfix,
+            stride=1, groups=1, kernel_size=1, padding=0):
+    """1x1 convolution"""
+    return [
+        ('{}_{}/conv'.format(module_name, postfix),
+         nn.Conv2D(in_channels, out_channels,
+                   kernel_size=kernel_size,
+                   stride=stride,
+                   padding=padding,
+                   groups=groups,
+                   bias_attr=False)),
+        ('{}_{}/norm'.format(module_name, postfix),
+         nn.BatchNorm2D(out_channels)),
+        ('{}_{}/relu'.format(module_name, postfix),
+         nn.ReLU()),
+    ]
+
+
+class _OSA_module(nn.Layer):
+    def __init__(self,
+                 in_ch,
+                 stage_ch,
+                 concat_ch,
+                 layer_per_block,
+                 module_name,
+                 identity=False):
+        super(_OSA_module, self).__init__()
+
+        self.identity = identity
+        self.layers = nn.LayerList()
+        in_channel = in_ch
+        for i in range(layer_per_block):
+            self.layers.append(nn.Sequential(
+                *conv3x3(in_channel, stage_ch, module_name, i)))
+            in_channel = stage_ch
+
+        # feature aggregation
+        in_channel = in_ch + layer_per_block * stage_ch
+        self.concat = nn.Sequential(
+            *(conv1x1(in_channel, concat_ch, module_name, 'concat')))
+
+    def forward(self, x):
+        identity_feat = x
+        output = []
+        output.append(x)
+        for layer in self.layers:
+            x = layer(x)
+            output.append(x)
+
+        x = paddle.concat(output, axis=1)
+        xt = self.concat(x)
+
+        if self.identity:
+            xt = xt + identity_feat
+
+        return xt
+
+
+class _OSA_stage(nn.Sequential):
+    def __init__(self,
+                 in_ch,
+                 stage_ch,
+                 concat_ch,
+                 block_per_stage,
+                 layer_per_block,
+                 stage_num):
+        super(_OSA_stage, self).__init__()
+
+        if not stage_num == 2:
+            self.add_sublayer('Pooling',
+                              nn.MaxPool2D(kernel_size=3, stride=2, ceil_mode=True))
+
+        module_name = f'OSA{stage_num}_1'
+        self.add_sublayer(module_name,
+                          _OSA_module(in_ch,
+                                      stage_ch,
+                                      concat_ch,
+                                      layer_per_block,
+                                      module_name))
+        for i in range(block_per_stage - 1):
+            module_name = f'OSA{stage_num}_{i + 2}'
+            self.add_sublayer(module_name,
+                              _OSA_module(concat_ch,
+                                          stage_ch,
+                                          concat_ch,
+                                          layer_per_block,
+                                          module_name,
+                                          identity=True))
+
+
+class VoVNet(nn.Layer):
+    def __init__(self,
+                 config_stage_ch,
+                 config_concat_ch,
+                 block_per_stage,
+                 layer_per_block,
+                 class_num=1000):
+        super(VoVNet, self).__init__()
+
+        # Stem module
+        stem = conv3x3(3, 64, 'stem', '1', 2)
+        stem += conv3x3(64, 64, 'stem', '2', 1)
+        stem += conv3x3(64, 128, 'stem', '3', 2)
+        self.add_sublayer('stem', nn.Sequential(*stem))
+
+        stem_out_ch = [128]
+        in_ch_list = stem_out_ch + config_concat_ch[:-1]
+        self.stage_names = []
+        for i in range(4):  # num_stages
+            name = 'stage%d' % (i + 2)
+            self.stage_names.append(name)
+            self.add_sublayer(name,
+                              _OSA_stage(in_ch_list[i],
+                                         config_stage_ch[i],
+                                         config_concat_ch[i],
+                                         block_per_stage[i],
+                                         layer_per_block,
+                                         i + 2))
+
+        self.classifier = nn.Linear(config_concat_ch[-1], class_num)
+
+        for m in self.sublayers():
+            if isinstance(m, nn.Conv2D):
+                kaiming_normal_(m.weight)
+            elif isinstance(m, (nn.BatchNorm2D, nn.GroupNorm)):
+                ones_(m.weight)
+                zeros_(m.bias)
+            elif isinstance(m, nn.Linear):
+                zeros_(m.bias)
+
+    def forward(self, x):
+        x = self.stem(x)
+        for name in self.stage_names:
+            x = getattr(self, name)(x)
+        x = F.adaptive_avg_pool2d(x, (1, 1)).flatten(1)
+        x = self.classifier(x)
+        return x
+
+
+def _vovnet(arch,
+            config_stage_ch,
+            config_concat_ch,
+            block_per_stage,
+            layer_per_block,
+            pretrained,
+            progress,
+            **kwargs):
+    model = VoVNet(config_stage_ch, config_concat_ch,
+                   block_per_stage, layer_per_block,
+                   **kwargs)
+    if pretrained:
+        state_dict = load_state_dict_from_url(model_urls[arch],
+                                              progress=progress)
+        model.load_state_dict(state_dict)
+    return model
+
+
+def vovnet57(pretrained=False, progress=True, **kwargs):
+    r"""Constructs a VoVNet-57 model as described in
+    `"An Energy and GPU-Computation Efficient Backbone Networks"
+    <https://arxiv.org/abs/1904.09730>`_.
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+        progress (bool): If True, displays a progress bar of the download to stderr
+    """
+    return _vovnet('vovnet57', [128, 160, 192, 224], [256, 512, 768, 1024],
+                   [1, 1, 4, 3], 5, pretrained, progress, **kwargs)
+
+
+def vovnet39(pretrained=False, progress=True, **kwargs):
+    r"""Constructs a VoVNet-39 model as described in
+    `"An Energy and GPU-Computation Efficient Backbone Networks"
+    <https://arxiv.org/abs/1904.09730>`_.
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+        progress (bool): If True, displays a progress bar of the download to stderr
+    """
+    return _vovnet('vovnet39', [128, 160, 192, 224], [256, 512, 768, 1024],
+                   [1, 1, 2, 2], 5, pretrained, progress, **kwargs)
+
+
+def vovnet27_slim(pretrained=False, progress=True, **kwargs):
+    r"""Constructs a VoVNet-39 model as described in
+    `"An Energy and GPU-Computation Efficient Backbone Networks"
+    <https://arxiv.org/abs/1904.09730>`_.
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+        progress (bool): If True, displays a progress bar of the download to stderr
+    """
+    return _vovnet('vovnet27_slim', [64, 80, 96, 112], [128, 256, 384, 512],
+                   [1, 1, 1, 1], 5, pretrained, progress, **kwargs)
