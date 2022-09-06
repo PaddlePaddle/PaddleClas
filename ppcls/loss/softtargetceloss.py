193fc609b0d1d34f5f0fe5b863c85e67b464ecbe
zengshao0622 <peter_z96@163.com>
2022-09-06 06:54:47 +0000

    update ppcls

---
+++ ppcls/loss/softtargetceloss.py
@@ -0,0 +1,14 @@
+import paddle 
+import paddle.nn as nn
+import paddle.nn.functional as F
+
+class SoftTargetCrossEntropy(nn.Layer):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x, target):
+        loss = paddle.sum(-target * F.log_softmax(x, axis=-1), axis=-1)
+        return {"SoftTargetCELoss": loss.mean()}
+    
+    def __str__(self,):
+        return type(self).__name__
