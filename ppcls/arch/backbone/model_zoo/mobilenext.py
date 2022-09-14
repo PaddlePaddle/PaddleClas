2c53f79dc6c8db0b079c3cac4e85272291ff7d12
Yang Nie <lenny.n@outlook.com>
2022-09-14 16:41:01 +0800

    remove docs of `_make_divisible`

--- ppcls/arch/backbone/model_zoo/mobilenext.py
+++ ppcls/arch/backbone/model_zoo/mobilenext.py
@@ -44,16 +44,6 @@ Linear = partial(nn.Linear, bias_attr=no_weight_decay_attr)
 
 
 def _make_divisible(v, divisor, min_value=None):
-    """
-    This function is taken from the original tf repo.
-    It ensures that all layers have a channel number that is divisible by 8
-    It can be seen here:
-    https://github.com/tensorflow/models/blob/master/research/slim/nets/mobilenet/mobilenet.py
-    :param v:
-    :param divisor:
-    :param min_value:
-    :return:
-    """
     if min_value is None:
         min_value = divisor
     new_v = max(min_value, int(v + divisor / 2) // divisor * divisor)
