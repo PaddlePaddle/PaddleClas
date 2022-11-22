2c46b116f55e93d6500b5f23da30b4a50f94417f
gaotingquan <gaotingquan@baidu.com>
2022-11-22 06:51:54 +0000

    update setup about classifiers

--- setup.py
+++ setup.py
@@ -50,14 +50,13 @@ setup(
         'swin-transformer', 'image-retrieval-system'
     ],
     classifiers=[
-        'Intended Audience :: Developers',
+        'Development Status :: 5 - Production/Stable',
         'Operating System :: OS Independent',
-        'Natural Language :: Chinese (Simplified)',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7', 'Topic :: Utilities'
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ], )
