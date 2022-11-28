eb13aaf017bab76916d83c68f148036ef9590baa
Fisher <fisheryung@outlook.com>
2022-11-28 11:02:49 +0800

    Export CaiT model

--- ppcls/arch/backbone/__init__.py
+++ ppcls/arch/backbone/__init__.py
@@ -71,6 +71,7 @@ from .model_zoo.peleenet import PeleeNet
 from .model_zoo.convnext import ConvNeXt_tiny
 from .model_zoo.nextvit import NextViT_small_224, NextViT_base_224, NextViT_large_224, NextViT_small_384, NextViT_base_384, NextViT_large_384
 from .model_zoo.cae import cae_base_patch16_224, cae_large_patch16_224
+from .model_zoo.cait import CaiT_M48, CaiT_M36, CaiT_S36, CaiT_S24, CaiT_S24_224, CaiT_XS24, CaiT_XXS24, CaiT_XXS24_224, CaiT_XXS36, CaiT_XXS36_224
 
 from .variant_models.resnet_variant import ResNet50_last_stage_stride1
 from .variant_models.resnet_variant import ResNet50_adaptive_max_pool2d
