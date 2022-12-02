9dcc508dc711f965690bf3aeb0b780773a0fd26e
zhangyubo0722 <zhangyubo0722@163.com>
2022-12-02 10:16:23 +0000

    add uniformer lst

--- ppcls/arch/backbone/__init__.py
+++ ppcls/arch/backbone/__init__.py
@@ -79,6 +79,7 @@ from .variant_models.pp_lcnet_variant import PPLCNet_x2_5_Tanh
 from .variant_models.pp_lcnetv2_variant import PPLCNetV2_base_ShiTu
 from .model_zoo.adaface_ir_net import AdaFace_IR_18, AdaFace_IR_34, AdaFace_IR_50, AdaFace_IR_101, AdaFace_IR_152, AdaFace_IR_SE_50, AdaFace_IR_SE_101, AdaFace_IR_SE_152, AdaFace_IR_SE_200
 from .model_zoo.wideresnet import WideResNet
+from .model_zoo.uniformer import UniFormer_small, UniFormer_small_plus, UniFormer_small_plus_dim64, UniFormer_base, UniFormer_base_ls
 
 
 # help whl get all the models' api (class type) and components' api (func type)
