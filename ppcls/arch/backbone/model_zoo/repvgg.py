14ddd75de7cb2c6870e4a713e824c9deb434ad93
zhangyubo0722 <94225063+zhangyubo0722@users.noreply.github.com>
2022-12-16 21:33:48 +0800

    Update repvgg.py

--- ppcls/arch/backbone/model_zoo/repvgg.py
+++ ppcls/arch/backbone/model_zoo/repvgg.py
@@ -20,7 +20,7 @@ import paddle
 import paddle.nn.functional as F
 import numpy as np
 
-#from ....utils.save_load import load_dygraph_pretrain, load_dygraph_pretrain_from_url
+from ....utils.save_load import load_dygraph_pretrain, load_dygraph_pretrain_from_url
 
 MODEL_URLS = {
     "RepVGG_A0":
