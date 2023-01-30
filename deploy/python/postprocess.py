3ad502e9fa716c1ce1796a152e723939ebffd04f
zhangyubo0722 <zhangyubo0722@163.com>
2023-01-30 14:09:31 +0000

    add aesthetic score predictor

--- deploy/python/postprocess.py
+++ deploy/python/postprocess.py
@@ -17,6 +17,7 @@ import copy
 import shutil
 from functools import partial
 import importlib
+import numpy
 import numpy as np
 import paddle
 import paddle.nn.functional as F
@@ -145,6 +146,22 @@ class ThreshOutput(object):
             return binary_classification(x)
         else:
             return multi_classification(x)
+
+
+class ScoreOutput(object):
+    def __init__(self, decimal_places):
+        self.decimal_places = decimal_places
+
+    def __call__(self, x, file_names=None):
+        paddle.set_printoptions(precision=self.decimal_places)
+        y = []
+        for idx, probs in enumerate(x):
+            score = x[0]
+            result = {"scores": score}
+            if file_names is not None:
+                result["file_name"] = file_names[idx]
+            y.append(result)
+        return y
 
 
 class Topk(object):
