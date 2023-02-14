9b1cbf4e3b7b19499061e8fd4cfacb110223e639
WenmuZhou <572459439@qq.com>
2023-02-14 13:56:55 +0000

    rename config_file_path to config_path

--- uapi/cls_task/runner.py
+++ uapi/cls_task/runner.py
@@ -17,34 +17,33 @@ from ..base import BaseRunner
 
 
 class ClsRunner(BaseRunner):
-    def train(self, config_file_path, cli_args, device):
+    def train(self, config_path, cli_args, device):
         python, _ = self.distributed(device)
         cli_args = " ".join(cli_args)
-        cmd = f"{python} tools/train.py -c {config_file_path} {cli_args}"
+        cmd = f"{python} tools/train.py -c {config_path} {cli_args}"
         self.run_cmd(cmd, switch_wdir=True, echo=True, silent=False)
 
-    def predict(self, config_file_path, cli_args, device):
+    def predict(self, config_path, cli_args, device):
         self.distributed(device)
         cli_args = " ".join(cli_args)
-        cmd = f"{self.python} tools/infer.py -c {config_file_path} {cli_args}"
+        cmd = f"{self.python} tools/infer.py -c {config_path} {cli_args}"
         self.run_cmd(cmd, switch_wdir=True, echo=True, silent=False)
 
-    def export(self, config_file_path, cli_args, device):
+    def export(self, config_path, cli_args, device):
         # `device` unused
         cli_args = " ".join(cli_args)
-        cmd = f"{self.python} tools/export_model.py -c {config_file_path}  {cli_args}"
+        cmd = f"{self.python} tools/export_model.py -c {config_path}  {cli_args}"
         self.run_cmd(cmd, switch_wdir=True, echo=True, silent=False)
 
-    def infer(self, config_file_path, cli_args, device):
+    def infer(self, config_path, cli_args, device):
         self.distributed(device)
         cli_args = " ".join(cli_args)
-        cmd = f"{self.python} python/predict_cls.py -c {config_file_path} {cli_args}"
+        cmd = f"{self.python} python/predict_cls.py -c {config_path} {cli_args}"
         self.run_cmd(cmd, switch_wdir='deploy', echo=True, silent=False)
 
-    def compression(self, config_file_path, cli_args, device, save_dir,
-                    model_name):
+    def compression(self, config_path, cli_args, device, save_dir, model_name):
         # Step 1: Train model
-        self.train(config_file_path, cli_args, device)
+        self.train(config_path, cli_args, device)
 
         # Step 2: Export model
         weight_path = os.path.join(save_dir, model_name, 'best_model')
@@ -52,4 +51,4 @@ class ClsRunner(BaseRunner):
             f'-o Global.pretrained_model={weight_path}',
             f'-o Global.save_inference_dir={save_dir}'
         ]
-        self.export(config_file_path, cli_args, device)
+        self.export(config_path, cli_args, device)
