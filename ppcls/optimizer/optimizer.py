3227fe152ec134c580e1014ccec63f1e27d961a3
Yang Nie <flytocc@outlook.com>
2023-04-08 23:10:14 +0800

    fix

--- ppcls/optimizer/optimizer.py
+++ ppcls/optimizer/optimizer.py
@@ -112,16 +112,16 @@ class Momentum(object):
     def __call__(self, model_list):
         # model_list is None in static graph
         parameters = None
-        if len(self.no_weight_decay_name_list) > 0:
+        if model_list:
             params_with_decay = []
             params_without_decay = []
             for m in model_list:
-                params = [p for n, p in m.named_parameters() \
-                          if not any(nd in n for nd in self.no_weight_decay_name_list)]
-                params_with_decay.extend(params)
-                params = [p for n, p in m.named_parameters() \
-                          if any(nd in n for nd in self.no_weight_decay_name_list) or (self.one_dim_param_no_weight_decay and len(p.shape) == 1)]
-                params_without_decay.extend(params)
+                for n, p in m.named_parameters():
+                    if any(nd in n for nd in self.no_weight_decay_name_list) \
+                        or (self.one_dim_param_no_weight_decay and len(p.shape) == 1):
+                        params_without_decay.append(p)
+                    else:
+                        params_with_decay.append(p)
             parameters = [{
                 "params": params_with_decay,
                 "weight_decay": self.weight_decay
@@ -129,9 +129,6 @@ class Momentum(object):
                 "params": params_without_decay,
                 "weight_decay": 0.0
             }]
-        else:
-            parameters = sum([m.parameters() for m in model_list],
-                             []) if model_list else None
         opt = optim.Momentum(
             learning_rate=self.learning_rate,
             momentum=self.momentum,
