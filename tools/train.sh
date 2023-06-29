f95f429dfaccaa100aff7f8c4ab8486de3433bbe
gaotingquan <gaotingquan@baidu.com>
2023-06-29 06:12:34 +0000

    rm specified python version 3.7

--- tools/train.sh
+++ tools/train.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # for single card train
-# python3.7 tools/train.py -c ./ppcls/configs/ImageNet/ResNet/ResNet50.yaml
+# python tools/train.py -c ./ppcls/configs/ImageNet/ResNet/ResNet50.yaml
 
 # for multi-cards train
 export CUDA_VISIBLE_DEVICES=0,1,2,3
-python3.7 -m paddle.distributed.launch --gpus="0,1,2,3" tools/train.py -c ./ppcls/configs/ImageNet/ResNet/ResNet50.yaml
+python -m paddle.distributed.launch --gpus="0,1,2,3" tools/train.py -c ./ppcls/configs/ImageNet/ResNet/ResNet50.yaml
