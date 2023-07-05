bf865cb0d849a728e2faa45c3ab38d08426551c7
gaotingquan <gaotingquan@baidu.com>
2023-07-05 08:53:12 +0000

    adapt to Pillow new version 10.0.0

--- deploy/utils/draw_bbox.py
+++ deploy/utils/draw_bbox.py
@@ -39,7 +39,7 @@ def draw_bbox_results(image,
         xmin, ymin, xmax, ymax = result["bbox"]
         text = "{}, {:.2f}".format(result["rec_docs"], result["rec_scores"])
         th = font_size
-        tw = font.getsize(text)[0]
+        tw = font.getlength(text)
         # tw = int(len(result["rec_docs"]) * font_size) + 60
         start_y = max(0, ymin - th)
 
