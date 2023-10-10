28c6c86d0255eabc1757213b22c2faf8403f03cd
zhangyubo0722 <zhangyubo0722@163.com>
2023-10-10 12:32:59 +0000

    modify save_load

--- ppcls/utils/save_load.py
+++ ppcls/utils/save_load.py
@@ -50,26 +50,35 @@ def _extract_student_weights(all_params, student_prefix="Student."):
     return s_params
 
 
-def load_dygraph_pretrain(model, 
+def _set_ssld_pretrained(pretrained_path,
+                         use_ssld=False,
+                         use_ssld_stage1_pretrained=False):
+    if use_ssld and "ssld" not in pretrained_path:
+        pretrained_path = pretrained_path.replace("_pretrained",
+                                                  "_ssld_pretrained")
+    if use_ssld_stage1_pretrained and "ssld" in pretrained_path:
+        pretrained_path = pretrained_path.replace("ssld_pretrained",
+                                                  "ssld_stage1_pretrained")
+    return pretrained_path
+
+
+def load_dygraph_pretrain(model,
                           pretrained_path,
                           use_ssld=False,
+                          use_ssld_stage1_pretrained=False,
                           use_imagenet22k_pretrained=False,
-                          use_imagenet22kto1k_pretrained=False,
-                          use_stage1_pretrained=False):
+                          use_imagenet22kto1k_pretrained=False):
     if pretrained_path.startswith(("http://", "https://")):
-        if not use_stage1_pretrained:
-            if use_ssld:
-                pretrained_path = pretrained_path.replace("_pretrained",
-                                                    "_ssld_pretrained")
-        else:
-            pretrained_path = pretrained_path.replace("ssld_pretrained",
-                                                "ssld_stage1_pretrained")
+        pretrained_path = _set_ssld_pretrained(
+            pretrained_path,
+            use_ssld=use_ssld,
+            use_ssld_stage1_pretrained=use_ssld_stage1_pretrained)
         if use_imagenet22k_pretrained:
             pretrained_path = pretrained_path.replace("_pretrained",
-                                                "_22k_pretrained")
+                                                      "_22k_pretrained")
         if use_imagenet22kto1k_pretrained:
             pretrained_path = pretrained_path.replace("_pretrained",
-                                                    "_22kto1k_pretrained")
+                                                      "_22kto1k_pretrained")
         pretrained_path = get_weights_path_from_url(pretrained_path)
     if not pretrained_path.endswith('.pdparams'):
         pretrained_path = pretrained_path + '.pdparams'
