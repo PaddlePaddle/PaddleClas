10fc41d1486132da444f1d7624e74e48e9253b03
Yang-ChangHui <71805205+Yang-Changhui@users.noreply.github.com>
2023-10-18 14:46:34 +0800

    【Hackathon 5th No.69】 分类大模型--人体视觉任务SOLIDER (#2995)
    * add_solider
    
    * add_solider
    
    * add_solider
    
    * add_solider
    
    * add_solider
    
    * add_solider
    
    * add_solider
    
    * add_solider
    
    * add_solider
    
    * add_solider
    
    * add_solider

--- ppcls/arch/backbone/legendary_models/swin_transformer.py
+++ ppcls/arch/backbone/legendary_models/swin_transformer.py
@@ -388,12 +388,8 @@ class SwinTransformerBlock(nn.Layer):
         self.window_size = window_size
         self.shift_size = shift_size
         self.mlp_ratio = mlp_ratio
-        if min(self.input_resolution) <= self.window_size:
-            # if window size is larger than input resolution, we don't partition windows
-            self.shift_size = 0
-            self.window_size = min(self.input_resolution)
-        assert 0 <= self.shift_size < self.window_size, "shift_size must in 0-window_size"
 
+        self.check_condition()
         self.norm1 = norm_layer(dim)
         self.attn = WindowAttention(
             dim,
@@ -902,7 +898,8 @@ def _load_pretrained(pretrained,
                      model_url,
                      use_ssld=False,
                      use_imagenet22k_pretrained=False,
-                     use_imagenet22kto1k_pretrained=False):
+                     use_imagenet22kto1k_pretrained=False,
+                     **kwargs):
     if pretrained is False:
         pass
     elif pretrained is True:
