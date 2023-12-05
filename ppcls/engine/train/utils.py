a50e137f75a1889172b017d35fcc073a29c8e57b
mmglove <guomengmeng01@baidu.com>
2023-12-05 11:07:15 +0800

    change unit

--- ppcls/engine/train/utils.py
+++ ppcls/engine/train/utils.py
@@ -63,9 +63,8 @@ def log_info(trainer, batch_size, epoch_id, iter_id):
     max_mem_reserved_msg = ""
     max_mem_allocated_msg = ""
     if paddle.device.is_compiled_with_cuda():
-        max_mem_reserved_msg = f"max_mem_reserved: {paddle.device.cuda.max_memory_reserved()} B"
-        max_mem_allocated_msg = f"max_mem_allocated: {paddle.device.cuda.max_memory_allocated()} B"
->>>>>>> d17336da37e04e1d91dbc3a386482d184986905c
+        max_mem_reserved_msg = f"max_mem_reserved: {paddle.device.cuda.max_memory_reserved() // (1024 ** 2)} MB"
+        max_mem_allocated_msg = f"max_mem_allocated: {paddle.device.cuda.max_memory_allocated() // (1024 ** 2)} MB"
     logger.info(
         f"[Train][Epoch {epoch_id}/{global_epochs}][Iter: {iter_id}/{trainer.iter_per_epoch}]{lr_msg}, {metric_msg}, {time_msg}, {ips_msg}, {eta_msg}, {max_mem_reserved_msg}, {max_mem_allocated_msg}"
     )
