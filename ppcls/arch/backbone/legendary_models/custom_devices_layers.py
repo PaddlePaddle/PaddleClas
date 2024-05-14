96b3f623aaa995a0dbccf0c2bb318fb27a8ff6d0
lujiafeng <lujiafeng@baidu.com>
2024-05-14 19:34:09 +0800

    AdaptiveAvgPool2D for NPU

---
+++ ppcls/arch/backbone/legendary_models/custom_devices_layers.py
@@ -0,0 +1,32 @@
+import paddle
+import paddle.nn as nn
+
+__all__ = ["AdaptiveAvgPool2D"]
+
+
+class AdaptiveAvgPool2D(nn.AdaptiveAvgPool2D):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        if paddle.device.get_device().startswith("npu"):
+            self.device = "npu"
+        else:
+            self.device = None
+
+        if isinstance(self._output_size, int) and self._output_size == 1:
+            self._gap = True
+        elif isinstance(self._output_size, tuple) and self._output_size[
+                0] == 1 and self._output_size[1] == 1:
+            self._gap = True
+        else:
+            self._gap = False
+
+    def forward(self, x):
+        if self.device == "npu" and self._gap:
+            # Global Average Pooling
+            N, C, _, _ = x.shape
+            x_mean = paddle.mean(x, axis=[2, 3])
+            x_mean = paddle.reshape(x_mean, [N, C, 1, 1])
+            return x_mean
+        else:
+            return super.forward(x)
