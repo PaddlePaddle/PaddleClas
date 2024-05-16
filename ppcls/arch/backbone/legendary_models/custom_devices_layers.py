a0eb1a895abaddbecbe6a204abb6ea39ae7661d3
lujiafeng <lujiafeng@baidu.com>
2024-05-16 11:25:49 +0800

    fix export error

--- ppcls/arch/backbone/legendary_models/custom_devices_layers.py
+++ ppcls/arch/backbone/legendary_models/custom_devices_layers.py
@@ -1,5 +1,6 @@
 import paddle
 import paddle.nn as nn
+import paddle.nn.functional as F
 
 __all__ = ["AdaptiveAvgPool2D"]
 
@@ -29,4 +30,8 @@ class AdaptiveAvgPool2D(nn.AdaptiveAvgPool2D):
             x_mean = paddle.reshape(x_mean, [N, C, 1, 1])
             return x_mean
         else:
-            return super().forward(x)
+            return F.adaptive_avg_pool2d(
+                x,
+                output_size=self._output_size,
+                data_format=self._data_format,
+                name=self._name, )
