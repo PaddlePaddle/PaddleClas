8347a93520b8044d945bca314f87a920e0c4c785
tianyuzhou668 <2431054748@qq.com>
2025-09-29 15:53:17 +0800

    [ILUVATAR_GPU] Support for iluvatar_gpu

--- ppcls/engine/engine.py
+++ ppcls/engine/engine.py
@@ -111,7 +111,7 @@ class Engine(object):
         # set device
         assert self.config["Global"]["device"] in [
             "cpu", "gpu", "xpu", "npu", "mlu", "dcu", "ascend", "intel_gpu",
-            "mps", "gcu"
+            "mps", "gcu", "iluvatar_gpu"
         ]
         self.device = paddle.set_device(self.config["Global"]["device"])
         logger.info('train with paddle {} and device {}'.format(
