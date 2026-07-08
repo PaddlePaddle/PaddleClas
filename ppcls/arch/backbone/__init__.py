1c112567c61e37c2ab39ec0e4396d69208e5c62f
learncat163 <learncat@163.com>
2026-07-08 12:04:55 +0800

    add efficientvit Base and Large (#3400)
    * 新增efficientvit的Base和Large模型的支持
    
    * fix Optimizer params

--- ppcls/arch/backbone/__init__.py
+++ ppcls/arch/backbone/__init__.py
@@ -330,6 +330,7 @@ from .model_zoo.adaface_ir_net import (
 )
 from .model_zoo.wideresnet import WideResNet
 from .model_zoo.uniformer import UniFormer_small, UniFormer_small_plus, UniFormer_small_plus_dim64, UniFormer_base, UniFormer_base_ls
+from .model_zoo.efficientvit import Efficientvit_B0, Efficientvit_B1, Efficientvit_B2, Efficientvit_B3, Efficientvit_L1, Efficientvit_L2, Efficientvit_L3
 from .model_zoo.fastvit import FastViT_T8, FastViT_T12, FastViT_SA12, FastViT_SA24, FastViT_SA36, FastViT_MA36
 from .model_zoo.edgenext import EdgeNeXt_XX_Small, EdgeNeXt_X_Small, EdgeNeXt_Small, EdgeNeXt_Base
 from .model_zoo.swiftformer import SwiftFormer_XS, SwiftFormer_S, SwiftFormer_L1, SwiftFormer_L3
